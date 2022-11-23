fbd68aba09d942edd861ff6e3ffc6d9866317373
Mark Marron <marron@microsoft.com>
2022-11-23 12:43:43 -0800

    yep

--- runtimes/javascript/compiler/body_emitter.ts
+++ runtimes/javascript/compiler/body_emitter.ts
@@ -1,1 +1,42 @@
 import * as assert from "assert";
+
+import { TIRLiteralBoolExpression, TIRLiteralIntegralExpression, TIRLiteralNoneExpression, TIRLiteralNothingExpression } from "../../../frontend/tree_ir/tir_body";
+
+class BodyEmitter {
+    constructor() {
+    }
+
+    private emitLiteralNoneExpression(exp: TIRLiteralNoneExpression): string {
+        return "null";
+    }
+
+    private emitLiteralNothingExpression(exp: TIRLiteralNothingExpression): string {
+        return "undefined";
+    }
+    private emitLiteralBoolExpression(exp: TIRLiteralBoolExpression): string {
+        return exp.value ? "true" : "false";
+    }
+
+    private emitLiteralIntegralExpression(exp: TIRLiteralIntegralExpression): string {
+        if(exp.etype === "Nat") {
+            xxxx;
+        }
+        else if(exp.etype === "Nat") {
+            xxxx;
+        }
+        else if(exp.etype === "BigNat") {
+            xxxx;
+        }
+        else {
+            assert(exp.etype === "BigInt", "Unknown type TIRLiteralIntegralExpression");
+        }
+    }
+
+    private emitLiteralRationalExpression = "LiteralRationalExpression",
+    private emitLiteralFloatPointExpression = "LiteralFloatExpression",
+    private emitLiteralRegexExpression = "LiteralRegexExpression",
+}
+
+export {
+    BodyEmitter
+};
