df5d7c78751d852ea58b990b93fc37b43d64b2d3
BosqueLanguage <83938164+BosqueLanguage@users.noreply.github.com>
2023-02-15 10:39:44 -0800

    Logical Not and Negation -- docs/tests
    Co-authored-by: Mark Marron <mark.marron@protonmail.com>

---
+++ test/expression/logical_not.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const expect = require("chai").expect;
+
+const {generatePaths, codegen, invokeExecutionOn, cleanTest} = require("../codeproc.js");
+
+describe('Logical /\\', function () {
+    const testopt = ["expression/logical_not", "notop"];
+    const { srcfile, dstdir, jsmain } = generatePaths(testopt);
+
+    before(function () { codegen(srcfile, dstdir); });
+    after(function () { cleanTest(dstdir); });
+
+    describe('!true', function () {
+        it('expected false', function () {
+            expect(invokeExecutionOn(jsmain, true)).to.eql(false);
+        });
+    });
+    describe('!false', function () {
+        it('expected true', function () {
+            expect(invokeExecutionOn(jsmain, false)).to.eql(true);
+        });
+    });
+});
+
