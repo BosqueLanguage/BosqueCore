de56842af7c76ac30ff4e423b9a5087b51748579
BosqueLanguage <83938164+BosqueLanguage@users.noreply.github.com>
2023-02-23 21:38:02 -0800

    nbody
    Co-authored-by: Mark Marron <mark.marron@protonmail.com>

--- src/runtimes/javascript/compiler/body_emitter.ts
+++ src/runtimes/javascript/compiler/body_emitter.ts
@@ -895,7 +895,7 @@ class BodyEmitter {
     private emitCreateCodePackExpression(exp: TIRCreateCodePackExpression): string {
         const capturepcdirect = exp.capturepackdirect.map((pcc) => `${pcc}: ${pcc}`);
         const capturepcindirect = exp.capturepackindirect.map((pcc) => `${pcc}: $CodePack.${pcc}`);
-        const capturevvdirect = exp.capturedirect.map((pcc) => `${pcc}: ${pcc}`);
+        const capturevvdirect = exp.capturedirect.map((pcc) => `${pcc}: ${pcc === "this" ? "$_this" : pcc}`);
         const capturevvindirect = exp.captureindirect.map((pcc) => `${pcc}: $CodePack.${pcc}`);
 
         return `{${[...capturevvdirect, ...capturevvindirect, ...capturepcdirect, ...capturepcindirect].join(", ")}}`;
