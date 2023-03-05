7cb949b840da1d64c707f3aec10f4ee2980abf65
Mark Marron <mark.marron@protonmail.com>
2023-03-04 22:22:50 -0800

    getting ready to write Bosque in Bosque

--- src/runtimes/javascript/cmd.ts
+++ src/runtimes/javascript/cmd.ts
@@ -18,7 +18,7 @@ const core_code = FS.readFileSync(core_path).toString();
 const runtime_code = FS.readFileSync(runtime_path).toString();
 const api_code = FS.readFileSync(api_path).toString();
 
-const fullargs = process.argv;
+let fullargs = process.argv;
 
 function workflowLoadUserSrc(files: string[]): CodeFileInfo[] {
     try {
@@ -116,24 +116,45 @@ function workflowEmitToDir(into: string, usercode: PackageConfig, corecode: stri
         assert(entrypoints.length === 1, "TODO: want to support multiple entrypoints later (at lease for Node.js packaging)");
         const epf = tasm.invokeMap.get(`${entrypoints[0].ns}::${entrypoints[0].fname}`) as TIRInvoke;
 
-        const loadlogic = "[" + epf.params.map((pp, ii) => `$API.bsqMarshalParse("${pp.type}", JSON.parse($API.cmdunescape(actual_args[${ii}].substring(1, actual_args[${ii}].length - 1))))`).join(", ") + "]";
-        const emitlogic = `$API.bsqMarshalEmit("${epf.resultType}", res_val)`;
+        if(fileargs) {
+            const loadlogic = "[" + epf.params.map((pp, ii) => `$API.bsqMarshalParse("${pp.type}", actual_args[${ii}])`).join(", ") + "]";
+            const emitlogic = `$API.bsqMarshalEmit("${epf.resultType}", res_val)`;
 
-        const mainf = Path.join(into, "_main_.mjs");
-        FS.writeFileSync(mainf, `"use strict";\n`
-            + `import * as $API from "./api.mjs";\n`
-            + `import * as $Main from "./Main.mjs";\n\n`
-            + `const actual_args = process.argv.slice(2);\n`
-            + `if(actual_args.length !== ${epf.params.length}) { process.stdout.write("error -- expected ${epf.params.length} args but got " + actual_args.length + " args\\n"); process.exit(0); }\n\n`
-            + `const bsq_args = ${loadlogic};\n`
-            + `try {\n`
-            + `    const res_val = $Main.main(...bsq_args);\n`
-            + `    const jres_val = ${emitlogic};\n`
-            + `    console.log(JSON.stringify(jres_val));\n`
-            + `} catch(ex) {\n`
-            + `    process.stdout.write("error -- " + ex.msg + "\\n");\n`
-            + `}\n`);
+            const mainf = Path.join(into, "_main_.mjs");
+            FS.writeFileSync(mainf, `"use strict";\n`
+                + `import * as FS from "fs";\n`
+                + `import * as $API from "./api.mjs";\n`
+                + `import * as $Main from "./Main.mjs";\n\n`
+                + `const actual_args = JSON.parse(FS.readFileSync(process.argv[2], "utf8"));\n`
+                + `if(!Array.isArray(actual_args) || actual_args.length !== ${epf.params.length}) { process.stdout.write("error -- expected ${epf.params.length} args\\n"); process.exit(0); }\n\n`
+                + `const bsq_args = ${loadlogic};\n`
+                + `try {\n`
+                + `    const res_val = $Main.main(...bsq_args);\n`
+                + `    const jres_val = ${emitlogic};\n`
+                + `    console.log(JSON.stringify(jres_val));\n`
+                + `} catch(ex) {\n`
+                + `    process.stdout.write("error -- " + ex.msg + "\\n");\n`
+                + `}\n`);
+        }
+        else {
+            const loadlogic = "[" + epf.params.map((pp, ii) => `$API.bsqMarshalParse("${pp.type}", JSON.parse($API.cmdunescape(actual_args[${ii}].substring(1, actual_args[${ii}].length - 1))))`).join(", ") + "]";
+            const emitlogic = `$API.bsqMarshalEmit("${epf.resultType}", res_val)`;
 
+            const mainf = Path.join(into, "_main_.mjs");
+            FS.writeFileSync(mainf, `"use strict";\n`
+                + `import * as $API from "./api.mjs";\n`
+                + `import * as $Main from "./Main.mjs";\n\n`
+                + `const actual_args = process.argv.slice(2);\n`
+                + `if(actual_args.length !== ${epf.params.length}) { process.stdout.write("error -- expected ${epf.params.length} args but got " + actual_args.length + " args\\n"); process.exit(0); }\n\n`
+                + `const bsq_args = ${loadlogic};\n`
+                + `try {\n`
+                + `    const res_val = $Main.main(...bsq_args);\n`
+                + `    const jres_val = ${emitlogic};\n`
+                + `    console.log(JSON.stringify(jres_val));\n`
+                + `} catch(ex) {\n`
+                + `    process.stdout.write("error -- " + ex.msg + "\\n");\n`
+                + `}\n`);
+        }
     } catch(e) {
         process.stderr.write(`JS emit error -- ${e}\n`);
         process.exit(1);
@@ -148,6 +169,11 @@ function buildJSDefault(into: string, srcfiles: string[]) {
     workflowEmitToDir(into, userpackage, core_code, runtime_code, api_code, "test", false, [{ns: "Main", fname: "main"}]);
 
     process.stdout.write("done!\n");
+}
+
+const fileargs = fullargs.includes("--fileargs");
+if(fileargs) {
+    fullargs = fullargs.filter((aa) => aa !== "--fileargs");
 }
 
 if(fullargs.length > 2 && fullargs[2] === "--outdir") {
