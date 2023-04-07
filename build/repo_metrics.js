da10e875e8f430dd131f1945fe562fa27fab4277
Mark Marron <mark.marron@protonmail.com>
2023-04-06 22:47:48 -0700

    bits

---
+++ build/repo_metrics.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const path = require("path");
+const fs = require("fs");
+
+function countLinesInFile(filepath, ext) {
+    if(!filepath.endsWith(ext)) {
+        return 0;
+    }
+    else {
+        const contents = fs.readFileSync(filepath, "utf8");
+        return (contents.match(/(^[ \t]*$)/gm) || [""]).length - 1;
+    }
+}
+
+function countLinesInDir(dir, ext) {
+    let total = 0;
+    const files = fs.readdirSync(dir);
+    for(const file of files) {
+        if(file.startsWith(".")) {
+            continue;
+        }
+
+        const filepath = path.join(dir, file);
+        const stat = fs.statSync(filepath);
+        if(stat.isDirectory()) {
+            total += countLinesInDir(filepath, ext);
+        }
+        else if(file.endsWith(ext)) {
+            total += countLinesInFile(filepath, ext);
+        }
+    }
+    return total;
+}
+
+["ts", "bsq", "smt2", "js", "h", "c", "cpp"].forEach((ext) => {
+    const total = countLinesInDir(path.join(__dirname, "..", "src"), ext);
+    const pad = " ".repeat(5 - ext.length);
+    console.log(`Total lines for ${ext}: ${pad}${total}`);
+});
