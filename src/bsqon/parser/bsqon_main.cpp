22fa227a0bac5a7fc67519b3ba92c9ae9a295cd9
Mark Marron <mark.marron@protonmail.com>
2023-11-23 22:29:22 -0500

    bits

--- src/bsqon/parser/bsqon_main.cpp
+++ src/bsqon/parser/bsqon_main.cpp
@@ -8,8 +8,8 @@
 int main(int argc, char** argv)
 {
     //argv is a file name to a JSON file
-    if(argc != 3) {
-        printf("Expected 2 arguments to a file containing the assembly/loadtype/and value\n");
+    if(argc != 4) {
+        printf("Expected 3 arguments to a file containing the assembly file, type, and value file\n");
         exit(1);
     }
 
@@ -27,29 +27,19 @@ int main(int argc, char** argv)
     }
     
     //the property assembly is the code so load it
-    if(jv["assembly"].is_null()) {
-        printf("Missing 'assembly' information\n");
-        exit(1);
-    }
-
     BSQON::AssemblyInfo assembly;
-    BSQON::AssemblyInfo::parse(jv["assembly"], assembly);
+    BSQON::AssemblyInfo::parse(jv, assembly);
 
     //the property loadtype is the type so look it up
-    if(!jv["loadtype"].is_string()) {
-        printf("Missing 'loadtype' information\n");
-        exit(1);
-    }
-
     const BSQON::Type* loadtype = nullptr;
-    loadtype = assembly.resolveType(jv["loadtype"].get<BSQON::TypeKey>());
+    loadtype = assembly.resolveType(argv[2]);
     if(loadtype->isUnresolved()) {
         printf("Invalid 'loadtype'\n");
         exit(1);
     }
 
     //the property value is the BSQON value (as a JSON string) so parse it
-    BSQON_AST_Node* node = parse_from_file(argv[2]);
+    BSQON_AST_Node* node = parse_from_file(argv[3]);
 
     BSQON::Parser parser(&assembly);
     BSQON::Value* res = parser.parseValue(loadtype, node);
