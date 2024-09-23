5d8b169e6ceba172596082160e6fc0d2be6bbf05
Mark Marron <marron@cs.uky.edu>
2024-09-22 23:06:09 -0400

    fixed

--- test/runtime/cons_exps/cons_type.test.js
+++ test/runtime/cons_exps/cons_type.test.js
@@ -22,7 +22,7 @@ describe ("Exec -- Type Alias w/ Invariant Constructor", () => {
 describe ("Exec -- type decl of strings w/ stacked constraints", () => {
     it("should exec string options type decl", function () {
         runMainCode('type SV2 = String of /[a-z]+ & [a-c]+/; public function main(): String { return SV2{"abc"}.value; }', ["abc", "String"]);
-        runMainCode("type SV2 = String of /[a-z]+ & [a-c]+/c; public function main(): CString { return SV2{'abc'}.value; }", ["abc", "CString"]);
+        runMainCode("type SV2 = CString of /[a-z]+ & [a-c]+/c; public function main(): CString { return SV2{'abc'}.value; }", ["abc", "CString"]);
 
         runMainCode('const re2: Regex = /[a-z]+/; type SV2 = String of /${Main::re2} & [a-c]+/; public function main(): String { return SV2{"abc"}.value; }', ["abc", "String"]);  
     });
@@ -30,7 +30,7 @@ describe ("Exec -- type decl of strings w/ stacked constraints", () => {
     it("should fail string constraints", function () {
         runMainCodeError('type SV2 = String of /[a-z]+ & [az]+/; public function main(): String { return SV2{"abc"}.value; }', 'Error -- failed regex @ test.bsq:3'); 
 
-        runMainCodeError('const re2: Regex = /[a-z]/; type SV2 = String of /${Main::re2} & [a-c]+/; public function main(): String { return SV2{"abc"}.value; }', "Error -- failed regex -- re2['Main::re2'] @ test.bsq:3");
+        runMainCodeError('const re2: Regex = /[a-z]/; type SV2 = String of /${Main::re2} & [a-c]+/; public function main(): String { return SV2{"abc"}.value; }', "Error -- failed regex @ test.bsq:3");
 
         runMainCodeError("const re2: CRegex = /[a-c]/c; type SV1 = CString of Main::re2; public function main(): CString { return SV1{'abc'}.value; }", "Error -- failed regex -- re2['Main::re2'] @ test.bsq:3");
     });
