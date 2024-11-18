9e19dc6fd9f59cf3e7331f7eb43738d04ad54175
Mark Marron <mark.marron@uky.edu>
2024-11-18 16:01:35 -0500

    sign

--- test/documentation/readme/allof.test.js
+++ test/documentation/readme/allof.test.js
@@ -1,12 +1,14 @@
 "use strict";
 
-import { checkTestFunctionInFileError, runMainCode } from "../../../bin/test/documentation/docs_nf.js";
+import { runMainCode } from "../../../bin/test/documentation/docs_nf.js";
 import { describe, it } from "node:test";
+
+const allpositive = 'function allPositive(...args: List<Int>): Bool { return args.allOf(pred(x) => x >= 0i); }';
 
 describe ("allof exec", () => {
     it("should exec allof", function () {
-        runMainCode('function allPositive(...args: List<Int>): Bool { return args.allOf(pred(x) => x >= 0i); } public function main(): Bool { return allPositive(1i, 3i, 4i); }', [true, "Bool"]); 
-        runMainCode('function allPositive(...args: List<Int>): Bool { return args.allOf(pred(x) => x >= 0i); } public function main(): Bool { return allPositive(); }', [true, "Bool"]);
-        runMainCode('function allPositive(...args: List<Int>): Bool { return args.allOf(pred(x) => x >= 0i); } public function main(): Bool { return allPositive(1i, 3i, -4i); }', [false, "Bool"]);
+        runMainCode(`${allpositive} public function main(): Bool { return allPositive(1i, 3i, 4i); }`, [true, "Bool"]); 
+        runMainCode(`${allpositive} public function main(): Bool { return allPositive(); }`, [true, "Bool"]);
+        runMainCode(`${allpositive} public function main(): Bool { return allPositive(1i, 3i, -4i); }`, [false, "Bool"]);
     });
 });
