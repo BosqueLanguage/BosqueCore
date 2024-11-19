8bfcea844221636721ffb7e004ba85643c93abb4
Mark Marron <mark.marron@uky.edu>
2024-11-19 14:12:29 -0500

    more

--- test/documentation/readme/type_alias.test.js
+++ test/documentation/readme/type_alias.test.js
@@ -1,14 +1,14 @@
 "use strict";
 
-import { checkTestFunctionInFileError, runMainCode } from "../../../bin/test/documentation/docs_nf.js";
+import { checkTestFunctionInFileError, runMainCode, runMainCodeError } from "../../../bin/test/documentation/docs_nf.js";
 import { describe, it } from "node:test";
 
 const decls = 'type Fahrenheit = Int; type Celsius = Int; type Percentage = Nat & { invariant $value <= 100n; } function isFreezing(temp: Celsius): Bool { return temp <= 0i<Celsius>; }'
 
 describe ("type alias typechecks", () => {
     it("should typefail operations", function () {
-        checkTestFunctionInFileError(`${decls} public function main(): Int { let x = 32i<Fahrenheit> + 0i<Celsius>; return 0i; }`, "err1");
-        checkTestFunctionInFileError(`${decls} public function main(): Bool { return isFreezing(5.0f); }`, "err2");
+        checkTestFunctionInFileError(`${decls} public function main(): Int { let x = 32i<Fahrenheit> + 0i<Celsius>; return 0i; }`, "Addition operator requires 2 arguments of the same type");
+        checkTestFunctionInFileError(`${decls} public function main(): Bool { return isFreezing(5.0f); }`, "Argument temp expected type Celsius but got Float");
     });
 });
 
@@ -21,6 +21,6 @@ describe ("type alias exec", () => {
     });
 
     it("should fail", function () {
-        runMainCode(`${decls} public function main(): Nat { return 101n<Percentage>.value; }`, [30n, "Nat"]); 
+        runMainCodeError(`${decls} public function main(): Nat { return 101n<Percentage>.value; }`, 'Error -- failed invariant @ test.bsq'); 
     });
 });
