ffddb46e5894b21ca9cb82d58a97527c3a86428d
Mark Marron <mark.marron@uky.edu>
2024-11-26 16:41:31 -0500

    more lists

---
+++ test/stdlib/list/list_filter.test.js
@@ -0,0 +1,29 @@
+"use strict";
+
+import { runMainCode } from "../../../bin/test/stdlib/stdlib_nf.js";
+import { describe, it } from "node:test";
+
+describe ("List -- filter basic", () => {
+    it("should do simple filters", function () {
+        runMainCode('public function main(): Nat { return List<Int>{}.filter(pred(x) => x >= 0i).size(); }', [0n, "Nat"]);
+        runMainCode('public function main(): Nat { return List<Int>{1i, 2i, 3i}.filter(pred(x) => false).size(); }', [0n, "Nat"]);
+        runMainCode('public function main(): Nat { return List<Int>{1i, 2i, 3i}.filter(pred(x) => true).size(); }', [3n, "Nat"]);
+
+        runMainCode('public function main(): Int { return List<Int>{1i}.filter(pred(x) => x >= 0i).front(); }', [1n, "Int"]);
+        runMainCode('public function main(): Int { return List<Int>{1i, -1i}.filter(pred(x) => x >= 0i).back(); }', [1n, "Int"]);
+    });
+});
+
+describe ("List -- filter index basic", () => {
+    it("should do simple filter index", function () {
+        runMainCode('public function main(): Nat { return List<Nat>{}.filterIdx(pred(x, i) => x >= i).size(); }', [0n, "Nat"]);
+        runMainCode('public function main(): Nat { return List<Int>{1i, 2i, 3i}.filterIdx(pred(x, i) => false).size(); }', [0n, "Nat"]);
+        runMainCode('public function main(): Nat { return List<Int>{1i, 2i, 3i}.filterIdx(pred(x, i) => true).size(); }', [3n, "Nat"]);
+
+        runMainCode('public function main(): Nat { return List<Nat>{1n}.filterIdx(pred(x, i) => x >= i).front(); }', [1n, "Nat"]);
+        runMainCode('public function main(): Nat { return List<Nat>{1n, 0n}.filterIdx(pred(x, i) => x >= i).back(); }', [1n, "Nat"]);
+
+        runMainCode('public function main(): Nat { return List<Nat>{0n, 1n, 2n}.filterIdx(pred(x, i) => x == i).size(); }', [3n, "Nat"]);
+        runMainCode('public function main(): Nat { return List<Nat>{0n, 1n, 2n}.filterIdx(pred(x, i) => x == i).back(); }', [2n, "Nat"]);
+    });
+});
--- test/stdlib/list/list_map.test.js
+++ test/stdlib/list/list_map.test.js
@@ -11,3 +11,14 @@ describe ("List -- map basic", () => {
         runMainCode('public function main(): Bool { return List<Int>{1i, -1i}.map<Bool>(fn(x) => x >= 0i).back(); }', [false, "Bool"]);
     });
 });
+
+describe ("List -- map index basic", () => {
+    it("should do simple maps index", function () {
+        runMainCode('public function main(): Nat { return List<Nat>{}.mapIdx<Bool>(fn(x, i) => x >= i).size(); }', [0n, "Nat"]);
+
+        runMainCode('public function main(): Bool { return List<Nat>{1n}.mapIdx<Bool>(fn(x, i) => x >= i).front(); }', [true, "Bool"]);
+        runMainCode('public function main(): Bool { return List<Nat>{1n, 0n}.mapIdx<Bool>(fn(x, i) => x >= i).back(); }', [false, "Bool"]);
+
+        runMainCode('public function main(): Nat { return List<Int>{1i, -1i, 3i}.mapIdx<Nat>(fn(x, i) => i).back(); }', [2n, "Nat"]);
+    });
+});
