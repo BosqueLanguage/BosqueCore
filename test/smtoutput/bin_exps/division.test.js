ebdf1d4c3eb34a2517848a2df2f2ec4c44e8d3c8
Mark Marron <mark.marron@protonmail.com>
2025-03-20 18:07:33 -0400

    yep

--- test/smtoutput/bin_exps/division.test.js
+++ test/smtoutput/bin_exps/division.test.js
@@ -5,14 +5,14 @@ import { describe, it } from "node:test";
 
 describe ("SMT evaluate -- Simple div", () => {
     it("should smt eval simple", function () {
-        runishMainCodeUnsat("public function main(x: Int): Int { return 10i // x; }", "(declare-const b (@Result Int)) (assert (= b (Main@main 5))) (assert (not (= b (@Result-ok 2))))");
+        runishMainCodeUnsat("public function main(x: Int): Int { return 10i // x; }", "(declare-const b (@Result Int)) (assert (= b (Main@main 5))) (assert (= b @Result-err-other))");
         runishMainCodeUnsat("public function main(x: Int): Int { return 10i // x; }", "(declare-const b (@Result Int)) (assert (= b (Main@main 0))) (assert (not (= b @Result-err-other)))");
     });
 });
 
 describe ("SMT check props -- Simple div", () => {
     it("should smt eval simple", function () {
-        checkProperties("public function main(x: Nat): Nat { return 10 // x; }", [{ pkey: ";;--FUNCTION_DECLS--;;", expected: "unk1" }]);
+        checkProperties("public function main(x: Nat): Nat { return 10n // x; }", [{ pkey: ";;--FUNCTION_DECLS--;;", expected: "(define-fun Main@main ((x Nat)) (@Result Nat) (ite (= x Nat@zero) (as @Result-err-other (@Result Nat)) (@Result-ok (@NLA_I_div 10 x))) )" }]);
     });
 });
 
--- test/smtoutput/bin_exps/mult.test.js
+++ test/smtoutput/bin_exps/mult.test.js
@@ -5,13 +5,13 @@ import { describe, it } from "node:test";
 
 describe ("SMT evaluate -- Simple mult", () => {
     it("should smt eval simple", function () {
-        runishMainCodeUnsat("public function main(x: Int): Int { return x * 2i; }", "(declare-const b Int) (assert (= b (Main@main 3))) (assert (not (= b 1)))");
+        runishMainCodeUnsat("public function main(x: Float): Float { return x * 2.0f; }", "(declare-const b Real) (assert (= b (Main@main 3))) (assert (not (= b 6.0)))");
     });
 });
 
 describe ("SMT check props -- Simple mult", () => {
     it("should smt eval simple", function () {
-        checkProperties("public function main(x: Nat): Nat { return x * 2n; }", [{ pkey: ";;--FUNCTION_DECLS--;;", expected: "unk2" }]);
+        checkProperties("public function main(x: Nat): Nat { return x * 2n; }", [{ pkey: ";;--FUNCTION_DECLS--;;", expected: "(define-fun Main@main ((x Nat)) Nat (@NLA_I_mult x 2) )" }]);
     });
 });
 
