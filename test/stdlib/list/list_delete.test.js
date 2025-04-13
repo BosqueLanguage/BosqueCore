4a750e197a29056c420adfbb7eb2b4a2eecea10d
1stub <stubbiegaming@gmail.com>
2025-04-13 12:24:56 -0400

    housekeeping

--- test/stdlib/list/list_delete.test.js
+++ test/stdlib/list/list_delete.test.js
@@ -13,15 +13,16 @@ describe ("List -- delete", () => {
         runMainCode('public function main(): Nat { return List<Int>{1i, 2i}.pushBack(3i).pushBack(4i).pushBack(5i).delete(0n).delete(0n).size(); }', "3n"); 
         runMainCode('public function main(): Int { return List<Int>{1i, 2i}.pushBack(3i).pushBack(4i).pushBack(5i).pushBack(6i).delete(0n).delete(0n).get(0n); }', "3i"); 
         runMainCode('public function main(): Nat { return List<Int>{4i, 5i}.pushFront(3i).pushFront(2i).pushFront(1i).pushBack(6i).pushBack(7i).delete(0n).delete(0n).delete(3n).size(); }', "4n"); 
-        runMainCode('public function main(): Nat { return List<Int>{1i, 2i}.delete(1n).size(); }', "1n"); 
-        runMainCode('public function main(): Nat { return List<Int>{1i, 2i}.delete(1n).size(); }', "1n"); 
+        runMainCode('public function main(): Int { return List<Int>{1i}.pushBack(2i).pushFront(3i).delete(0n).front(); }', "1i"); 
+        runMainCode('public function main(): Int { return List<Int>{1i, 2i}.delete(1n).get(0n); }', "1i"); 
     });
     it("should delete to empty", function() {
         runMainCode('public function main(): Nat { return List<Int>{1i, 2i}.delete(1n).delete(0n).size(); }', "0n"); 
         runMainCode('public function main(): Bool { return List<Int>{1i, 4i}.delete(0n).delete(0n).empty(); }', "true");
-        runMainCode('public function main(): Bool { return List<Int>{1i, 3i}.pushBack(0i).pushFront(9i).pushBack(6i).delete(0n).delete(0n).delete(0n).delete(0n).empty(); }', "true");
+        runMainCode('public function main(): Bool { return List<Int>{1i, 3i}.pushBack(0i).pushFront(9i).pushBack(6i).delete(0n).delete(0n).delete(0n).delete(0n).delete(0n).empty(); }', "true");
     });
     it("should fail invaid index", function() {
         runMainCodeError('public function main(): Nat { return List<Int>{1i, 2i}.delete(1n).delete(10n).size(); }', "Error -- i <= this.size() @ list.bsq"); 
+        runMainCodeError('public function main(): Nat { return List<Int>{1i, 3i}.pushBack(2i).delete(4n).size(); }', "Error -- i <= this.size() @ list.bsq"); 
     });
 });
