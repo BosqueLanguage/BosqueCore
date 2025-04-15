a57bbf96868dbb2ffb8942d0aa70b35d8d695776
Mark Marron <marron@cs.uky.edu>
2025-04-15 12:46:10 -0400

    Adding more SMT support (#195)

--- src/frontend/bsqir_emit.ts
+++ src/frontend/bsqir_emit.ts
@@ -200,7 +200,7 @@ class BSQIREmitter {
             return `BSQAssembly::LambdaTypeSignature{ ${tsbase}, frecursive=${recinfo}, isPredLambda=${ispred}, params=List<BSQAssembly::LambdaParameterSignature>{${tparams}}, resultType=${tret} }`;
         }
         else {
-            assert(false, "Unknown type signature");
+            assert(false, "Unknown type signature " + ttype.tkeystr);
         }
     }
 
@@ -262,11 +262,11 @@ class BSQIREmitter {
     }
     
     private emitLiteralCStringExpression(exp: LiteralSimpleExpression): string {
-        return `BSQAssembly::LiteralCStringExpression{ ${this.emitExpressionBase(exp)}, value='${exp.value}' }`;
+        return `BSQAssembly::LiteralCStringExpression{ ${this.emitExpressionBase(exp)}, value=${exp.value} }`;
     }
 
     private emitLiteralStringExpression(exp: LiteralSimpleExpression): string {
-        return `BSQAssembly::LiteralStringExpression{ ${this.emitExpressionBase(exp)}, value='${exp.value}' }`;
+        return `BSQAssembly::LiteralStringExpression{ ${this.emitExpressionBase(exp)}, value=${exp.value} }`;
     }
  
     private emitLiteralTypeDeclValueExpression(exp: LiteralTypeDeclValueExpression): string {
@@ -1051,7 +1051,7 @@ class BSQIREmitter {
     
     private emitVariableInitializationStatement(stmt: VariableInitializationStatement): string {
         const sbase = this.emitStatementBase(stmt);
-        const vtype = this.emitTypeSignature(stmt.vtype);
+        const vtype = this.emitTypeSignature(stmt.actualtype as TypeSignature);
         const rhsexp = this.emitExpressionRHS(stmt.exp);
         
         return `BSQAssembly::VariableInitializationStatement{ ${sbase}, name='${stmt.name}'<BSQAssembly::Identifier>, vtype=${vtype}, exp=${rhsexp} }`;
