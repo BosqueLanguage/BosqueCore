01ade7cd3b6bd05b44ec278d8b709ecd84a9f243
Mark Marron <mark.marron@uky.edu>
2025-04-16 15:12:11 -0400

    yes

--- test/smtoutput/bin_exps/division.test.js
+++ test/smtoutput/bin_exps/division.test.js
@@ -7,12 +7,14 @@ describe ("SMT evaluate -- Simple div", () => {
     it("should smt eval simple", function () {
         runishMainCodeUnsat("public function main(x: Int): Int { return 10i // x; }", "(declare-const b (@Result Int)) (assert (= b (Main@main 5))) (assert (= b @Result-err-other))");
         runishMainCodeUnsat("public function main(x: Int): Int { return 10i // x; }", "(declare-const b (@Result Int)) (assert (= b (Main@main 0))) (assert (not (= b @Result-err-other)))");
+
+        runishMainCodeUnsat("public function main(x: Int): Int { return x // 2i; }", "(declare-const b (@Result Int)) (assert (= b (Main@main 8))) (assert (not (= b (@Result-ok 4))))");
     });
 });
 
 describe ("SMT check props -- Simple div", () => {
     it("should smt eval simple", function () {
-        checkProperties("public function main(x: Nat): Nat { return 10n // x; }", [{ pkey: ";;--FUNCTION_DECLS--;;", expected: "(define-fun Main@main ((x Nat)) (@Result Nat) (ite (= x Nat@zero) (as @Result-err-other (@Result Nat)) (@Result-ok (@NLA_I_div 10 x))) )" }]);
+        checkProperties("public function main(x: Nat): Nat { return 10n // x; }", [{ pkey: ";;--FUNCTION_DECLS--;;", expected: "(define-fun Main@main ((x Nat)) (@Result Nat) (ite (= x Nat@zero) (as @Result-err-other (@Result Nat)) (@Result-ok (/ 10 x))) )" }]);
     });
 });
 
