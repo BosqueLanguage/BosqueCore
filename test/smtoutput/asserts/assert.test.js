d98a89cc5b6e3a119f40bc76f2c10ca3e95ae08e
Mark Marron <mark.marron@uky.edu>
2025-04-17 10:06:10 -0400

    eq code

--- test/smtoutput/asserts/assert.test.js
+++ test/smtoutput/asserts/assert.test.js
@@ -14,7 +14,15 @@ describe ("SMT -- simple assert", () => {
         runishMainCodeUnsat("public function main(): Int { assert true; return 1i; }", "(declare-const a Int) (assert (= a Main@main)) (assert (not (= 1 a)))");
     });
 
-    it("should SMT eval error assert (fail)", function () {
+    it("should SMT eval with assert (ok)", function () {
+        runishMainCodeUnsat("public function main(x: Int): Int { assert x != 1i; return x; }", "(declare-const a (@Result Int)) (assert (= a (Main@main 2))) (assert (not (= (@Result-ok 2) a)))");
+    });
+
+    it("should SMT eval simple assert (fail)", function () {
         runishMainCodeUnsat("public function main(): Int { assert false; return 1i; }", "(declare-const a (@Result Int)) (assert (= a Main@main)) (assert (not (= @Result-err-other a)))");
+    });
+
+    it("should SMT eval with assert (fail)", function () {
+        runishMainCodeUnsat("public function main(x: Int): Int { assert x != 1i; return x; }", "(declare-const a (@Result Int)) (assert (= a (Main@main 1))) (assert (not (= @Result-err-other a)))");
     });
 });
