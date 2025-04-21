72b6d327507031d97c6bc40ff588dd7512d03c92
Mark Marron <marron@cs.uky.edu>
2025-04-20 22:15:59 -0400

    Compiler and SMT support for UnaryExpression (#198)

--- test/runtime/unary_exps/sign.test.js
+++ test/runtime/unary_exps/sign.test.js
@@ -4,8 +4,13 @@ import { runMainCode } from "../../../bin/test/runtime/runtime_nf.js";
 import { describe, it } from "node:test";
 
 describe ("Exec -- Simple numeric sign", () => {
-    it("should check simple not", function () {
+    it("should exec (simplify) simple sign", function () {
         runMainCode("public function main(): Int { return -(3i); }", "-3i");
         runMainCode("public function main(): Nat { return +(5n); }", "5n");
+    });
+
+    it("should exec simple sign", function () {
+        runMainCode("public function main(): Int { let x = 3i; return -x; }", "-3i");
+        runMainCode("public function main(): Nat { let x = 5n; return +x; }", "5n");
     });
 });
---
+++ test/smtoutput/unary_exps/not.test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import { runishMainCodeUnsat } from "../../../bin/test/smtoutput/smtemit_nf.js";
+import { describe, it } from "node:test";
+
+describe ("SMT -- Simple Boolean not", () => {
+    it("should SMT exec simple not", function () {
+        runishMainCodeUnsat("public function main(x: Bool): Bool { return !x; }", "(assert (not (Main@main false)))");
+        runishMainCodeUnsat("public function main(x: Bool): Bool { return !!x; }", "(assert (not (Main@main true)))");
+    });
+});
