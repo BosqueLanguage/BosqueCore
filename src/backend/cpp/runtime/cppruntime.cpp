68c2d1cb52d08eefa8fa2efd2294ddf8963fd01c
1stub <stubbiegaming@gmail.com>
2025-04-24 10:53:21 -0400

    added backend for charbuffers

--- src/backend/cpp/runtime/cppruntime.cpp
+++ src/backend/cpp/runtime/cppruntime.cpp
@@ -1,17 +1,70 @@
 #include "cppruntime.hpp"
 
-PathStack PathStack::create() const {
-    return {0};
+PathStack PathStack::create() {
+    return {0, 0};
 }
-
 PathStack PathStack::left() const {
     return { bits << 1 };
 }
-
 PathStack PathStack::right() const {
     return { bits << 1 | 1 };
 }
-
 PathStack PathStack::up() const {
     return { bits >> 1 };
+}
+
+CCharBuffer CCharBuffer::create_empty() {
+    return {{}, 0};
+}
+CCharBuffer CCharBuffer::create_1(uint8_t c1) {
+    return {{c1}, 1};
+}
+CCharBuffer CCharBuffer::create_2(uint8_t c1, uint8_t c2) {
+    return {{c1, c2}, 2};
+}
+CCharBuffer CCharBuffer::create_3(uint8_t c1, uint8_t c2, uint8_t c3) {
+    return {{c1, c2, c3}, 3};
+}
+CCharBuffer CCharBuffer::create_4(uint8_t c1, uint8_t c2, uint8_t c3, uint8_t c4) {
+    return {{c1, c2, c3, c4}, 4};
+}
+CCharBuffer CCharBuffer::create_5(uint8_t c1, uint8_t c2, uint8_t c3, uint8_t c4, uint8_t c5) {
+    return {{c1, c2, c3, c4, c5}, 5};
+}
+CCharBuffer CCharBuffer::create_6(uint8_t c1, uint8_t c2, uint8_t c3, uint8_t c4, uint8_t c5, uint8_t c6) {
+    return {{c1, c2, c3, c4, c5, c6}, 6};
+}
+CCharBuffer CCharBuffer::create_7(uint8_t c1, uint8_t c2, uint8_t c3, uint8_t c4, uint8_t c5, uint8_t c6, uint8_t c7) {
+    return {{c1, c2, c3, c4, c5, c6, c7}, 7};
+}
+CCharBuffer CCharBuffer::create_8(uint8_t c1, uint8_t c2, uint8_t c3, uint8_t c4, uint8_t c5, uint8_t c6, uint8_t c7, uint8_t c8) {
+    return {{c1, c2, c3, c4, c5, c6, c7, c8}, 8};
+}
+
+UnicodeCharBuffer UnicodeCharBuffer::create_empty() {
+    return {{}, 0};
+}
+UnicodeCharBuffer UnicodeCharBuffer::create_1(uint32_t c1) {
+    return {{c1}, 1};
+}
+UnicodeCharBuffer UnicodeCharBuffer::create_2(uint32_t c1, uint32_t c2) {
+    return {{c1, c2}, 2};
+}
+UnicodeCharBuffer UnicodeCharBuffer::create_3(uint32_t c1, uint32_t c2, uint32_t c3) {
+    return {{c1, c2, c3}, 3};
+}
+UnicodeCharBuffer UnicodeCharBuffer::create_4(uint32_t c1, uint32_t c2, uint32_t c3, uint32_t c4) {
+    return {{c1, c2, c3, c4}, 4};
+}
+UnicodeCharBuffer UnicodeCharBuffer::create_5(uint32_t c1, uint32_t c2, uint32_t c3, uint32_t c4, uint32_t c5) {
+    return {{c1, c2, c3, c4, c5}, 5};
+}
+UnicodeCharBuffer UnicodeCharBuffer::create_6(uint32_t c1, uint32_t c2, uint32_t c3, uint32_t c4, uint32_t c5, uint32_t c6) {
+    return {{c1, c2, c3, c4, c5, c6}, 6};
+}
+UnicodeCharBuffer UnicodeCharBuffer::create_7(uint32_t c1, uint32_t c2, uint32_t c3, uint32_t c4, uint32_t c5, uint32_t c6, uint32_t c7) {
+    return {{c1, c2, c3, c4, c5, c6, c7}, 7};
+}
+UnicodeCharBuffer UnicodeCharBuffer::create_8(uint32_t c1, uint32_t c2, uint32_t c3, uint32_t c4, uint32_t c5, uint32_t c6, uint32_t c7, uint32_t c8) {
+    return {{c1, c2, c3, c4, c5, c6, c7, c8}, 8};
 }
--- src/backend/cpp/runtime/cppruntime.hpp
+++ src/backend/cpp/runtime/cppruntime.hpp
@@ -7,7 +7,7 @@ struct PathStack {
     uint64_t bits;
     int index;
 
-    PathStack create() const;
+    static PathStack create();
     PathStack left() const;
     PathStack right() const;
     PathStack up() const;
@@ -15,15 +15,31 @@ struct PathStack {
 
 // We say for now no more than 8 chars, may want to make this dynamically pick 8 or 16 max
 struct CCharBuffer {
-    uint8_t chars[16];
-    int index;
+    uint8_t chars[8];
+    int size;
 
-    // Would be some functions here to modify this buffer
+    static CCharBuffer create_empty();
+    static CCharBuffer create_1(uint8_t c1);
+    static CCharBuffer create_2(uint8_t c1, uint8_t c2);
+    static CCharBuffer create_3(uint8_t c1, uint8_t c2, uint8_t c3);
+    static CCharBuffer create_4(uint8_t c1, uint8_t c2, uint8_t c3, uint8_t c4);
+    static CCharBuffer create_5(uint8_t c1, uint8_t c2, uint8_t c3, uint8_t c4, uint8_t c5);
+    static CCharBuffer create_6(uint8_t c1, uint8_t c2, uint8_t c3, uint8_t c4, uint8_t c5, uint8_t c6);
+    static CCharBuffer create_7(uint8_t c1, uint8_t c2, uint8_t c3, uint8_t c4, uint8_t c5, uint8_t c6, uint8_t c7);
+    static CCharBuffer create_8(uint8_t c1, uint8_t c2, uint8_t c3, uint8_t c4, uint8_t c5, uint8_t c6, uint8_t c7, uint8_t c8);
 };
 
 struct UnicodeCharBuffer {
-    uint32_t chars[16];
-    int index;
+    uint32_t chars[8];
+    int size;
 
-    // Would be some functions here to modify this buffer
+    static UnicodeCharBuffer create_empty();
+    static UnicodeCharBuffer create_1(uint32_t c1);
+    static UnicodeCharBuffer create_2(uint32_t c1, uint32_t c2);
+    static UnicodeCharBuffer create_3(uint32_t c1, uint32_t c2, uint32_t c3);
+    static UnicodeCharBuffer create_4(uint32_t c1, uint32_t c2, uint32_t c3, uint32_t c4);
+    static UnicodeCharBuffer create_5(uint32_t c1, uint32_t c2, uint32_t c3, uint32_t c4, uint32_t c5);
+    static UnicodeCharBuffer create_6(uint32_t c1, uint32_t c2, uint32_t c3, uint32_t c4, uint32_t c5, uint32_t c6);
+    static UnicodeCharBuffer create_7(uint32_t c1, uint32_t c2, uint32_t c3, uint32_t c4, uint32_t c5, uint32_t c6, uint32_t c7);
+    static UnicodeCharBuffer create_8(uint32_t c1, uint32_t c2, uint32_t c3, uint32_t c4, uint32_t c5, uint32_t c6, uint32_t c7, uint32_t c8);
 };
