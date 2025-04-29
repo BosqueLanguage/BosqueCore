dec9de2d3aa16e0179e8dc226ba8284431d5f66e
1stub <stubbiegaming@gmail.com>
2025-04-29 15:09:41 -0400

    cppemit now takes in bsqasm, cant seem to get a file spat out though

--- src/cmd/analyzecpp.ts
+++ src/cmd/analyzecpp.ts
@@ -1,9 +1,14 @@
 import * as fs from "fs";
-
-/*
-import { Assembly } from "../frontend/assembly.js";
 import * as path from "path";
 import { fileURLToPath } from 'url';
+import { Assembly } from "../frontend/assembly.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const bosque_dir: string = path.join(__dirname, "../../../");
+const outdir: string = path.join(bosque_dir, "bin/cpp");
+/*
+import { Assembly } from "../frontend/assembly.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -13,10 +18,10 @@ const bosque_dir: string = path.join(__dirname, "../../../");
 // Doesnt really do much but spit out a file at outname directory for now
 // Would be good to call this generateCPPFile somewhere internally to this file
 // then export the wrapper method 
-function generateCPPFile(cppcomponents: string, outname: string) {
-    let code: string = cppcomponents;
+function generateCPPFile(asm: Assembly) {
 
-    fs.writeFileSync(outname, code);
+
+    fs.writeFileSync(outdir, "");
 }
 
 export { generateCPPFile };
