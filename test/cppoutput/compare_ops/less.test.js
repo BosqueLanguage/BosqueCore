fce33c6d6f0960e28a76adeaefaf44348a3ac227
1stub <stubbiegaming@gmail.com>
2025-05-19 11:25:17 -0400

    comparison testing

---
+++ test/cppoutput/compare_ops/less.test.js
@@ -0,0 +1,22 @@
+"use strict";
+
+import { runMainCode } from "../../../bin/test/cppoutput/cppemit_nf.js"
+import { describe, it } from "node:test";
+
+describe ("CPP Emit Evaluate -- basic <", () => {
+    it("should exec compare simple types", function () {
+        runMainCode("public function main(): Bool { return 0n < 1n; }", "true");
+        runMainCode("public function main(): Bool { return +2i < -2i; }", "false");
+
+        runMainCode("public function main(): Bool { return 1n < 1n; }", "false");
+    });
+});
+
+describe ("CPP Emit Evaluate -- basic <=", () => {
+    it("should exec compare simple types", function () {
+        runMainCode("public function main(): Bool { return 0n <= 1n; }", "true");
+        runMainCode("public function main(): Bool { return +2i <= -2i; }", "false");
+
+        runMainCode("public function main(): Bool { return 1n <= 1n; }", "true");
+    });
+});
