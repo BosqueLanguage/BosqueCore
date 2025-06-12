bc2024c8532bc91c4b1ea5cc19fea27a0990fb90
1stub <stubbiegaming@gmail.com>
2025-06-12 09:19:47 -0400

    more templated entities

--- test/cppoutput/mcall_exps/simple_resolved.test.js
+++ test/cppoutput/mcall_exps/simple_resolved.test.js
@@ -18,11 +18,20 @@ describe ("CPP Emit Evaluate -- entity methods", () => {
         runMainCode('entity Foo { field f: Int; method foo(x: Int, y: Int): Int { return this.f + x + y; }} public function main(): Int { return Foo{3i}.foo(x=1i,y=2i); }', "6_i"); 
     });
 
-    // Will need to add more comprehensive testing here
     it("should exec simple entity methods with template args", function () {
         runMainCode('entity Foo { method foo<T>(x: T, y: T): T { return y; }} public function main(): Int { return Foo{}.foo<Int>(1i, 2i); }', "2_i");
     });
     it("should exec simple entity methods with multiple template args", function () {
         runMainCode('entity Foo { method foo<T, K>(x: T, y: K): T { return x; }} public function main(): Int { return Foo{}.foo<Int, Nat>(2i, 1n); }', "2_i");
     });
+    it("should exec simple entity methods with type template", function () {
+        runMainCode('entity Foo<T> { field f: T; method foo(x: T): T { return if (true) then x else this.f; }} public function main(): Int { let x = Foo<Int>{3i}; return x.foo(2i); }', "2_i"); 
+    });
+
+    /*
+    it("should exec simple entity methods with both template", function () {
+        runMainCode('entity Foo<T> { field f: T; method foo<U>(): Bool { return this.f?<U>; }} public function main(): Bool { let x = Foo<Int>{3i}; return x.foo<Nat>(); }', "false"); 
+        runMainCode('entity Foo<T> { field f: T; method foo<U>(): Bool { return this.f?<U>; }} public function main(): Bool { let x = Foo<Int>{3i}; return x.foo<Int>(); }', "true"); 
+    });
+    */
 });
