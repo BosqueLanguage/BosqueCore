e86e4f5a06f908b739e525567b825260f88b383c
1stub <stubbiegaming@gmail.com>
2025-06-29 15:41:34 -0400

    fixed elif case

--- test/cppoutput/mcall_exps/simple_resolved.test.js
+++ test/cppoutput/mcall_exps/simple_resolved.test.js
@@ -28,13 +28,11 @@ describe ("CPP Emit Evaluate -- entity methods", () => {
         runMainCode('entity Foo<T> { field f: T; method foo(x: T): T { return if (true) then x else this.f; }} public function main(): Int { let x = Foo<Int>{3i}; return x.foo(2i); }', "2_i"); 
     });
 
-    /*
     it("should exec simple entity methods with both template", function () {
         runMainCode('entity Foo<T> { field f: T; method foo<U>(): Bool { return this.f?<U>; }} public function main(): Bool { let x = Foo<Int>{3i}; return x.foo<Nat>(); }', "false"); 
         runMainCode('entity Foo<T> { field f: T; method foo<U>(): Bool { return this.f?<U>; }} public function main(): Bool { let x = Foo<Int>{3i}; return x.foo<Int>(); }', "true"); 
     });
-    */
-});
+ });
 
 describe ("CPP Emit Evaluate -- eADT methods", () => {
     it("should exec simple eADT methods", function () {
@@ -46,13 +44,11 @@ describe ("CPP Emit Evaluate -- eADT methods", () => {
         runMainCode('datatype Foo<T> of Foo1 { field f: T; method foo(x: T): T { return if (true) then x else this.f; }} ; public function main(): Int { let x = Foo1<Int>{3i}; return x.foo(2i); }', "2_i"); 
     });
    
-    // Once we get explicitify supporting datatypes we this test should be good
-    /*
+   
     it("should exec simple eADT methods with template", function () {
         runMainCode('datatype Foo of Foo1 { field f: Int; method foo<T>(): Bool { return this.f?<T>; }} ; public function main(): Bool { let x = Foo1{3i}; return x.foo<Nat>(); }', "false"); 
         runMainCode('datatype Foo of Foo1 { field f: Int; method foo<T>(): Bool { return this.f?<T>; }} ; public function main(): Bool { let x = Foo1{3i}; return x.foo<Int>(); }', "true"); 
     });
-    */
     
 /*
     it("should exec simple ROOT eADT methods", function () {
