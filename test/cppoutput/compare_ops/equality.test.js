4398a129d56899efc9a35b8f97de55ff280e3217
1stub <stubbiegaming@gmail.com>
2025-07-01 10:23:19 -0400

    base for enum emit

--- test/cppoutput/compare_ops/equality.test.js
+++ test/cppoutput/compare_ops/equality.test.js
@@ -16,3 +16,13 @@ describe("CPP Emit Evalutate -- basic !equals", () => {
         runMainCode("public function main(): Bool { return +2i != 2i; }", "false");
     })
 });
+
+describe ("CPP Emit Evaluate -- enum strict equals", () => {
+    it("should exec enum strict equals operations", function () {
+        runMainCode("enum Foo { f, g } public function main(): Bool { return Foo#f === Foo#f; }", "true");
+        runMainCode("enum Foo { f, g } public function main(): Bool { return Foo#f !== Foo#f; }", "false");
+
+        runMainCode("enum Foo { f, g } public function main(): Bool { return Foo#f === Foo#g; }", "false");
+        runMainCode("enum Foo { f, g } public function main(): Bool { return Foo#f !== Foo#g; }", "true");
+    });
+});
