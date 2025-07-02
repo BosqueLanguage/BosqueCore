76f6708da5985b09a0b2952427f898310203ef19
1stub <stubbiegaming@gmail.com>
2025-07-02 08:57:08 -0400

    basic if binder

--- test/cppoutput/if_stmts/simple_if.test.js
+++ test/cppoutput/if_stmts/simple_if.test.js
@@ -13,4 +13,14 @@ describe ("CPP Emit Evaluate -- If Statement", () => {
         runMainCode("public function main(): Int { let x: Option<Int> = some(3i); if(x)some { return 3i; } return 1i; }", "3_i");
         runMainCode("public function main(): Int { let x: Option<Int> = none; if(x)some { return 3i; } return 1i; }", "1_i");
     });
+
+    it("should exec binder itest ifs", function () {
+        runMainCode("public function main(): Int { let x: Option<Int> = some(3i); if(x)@some { return $x; } return 1i; }", "3_i");
+        runMainCode("public function main(): Int { let x: Option<Int> = some(3i); if($y = x)@some { return $y; } return 1i; }", "3_i");
+    });
+
+    it("should exec binder & reflow itest ifs", function () {
+        runMainCode("public function main(): Int { let x: Option<Int> = some(3i); if(x)@@!some { return 0i; } return x; }", "3_i");
+        runMainCode("public function main(): Int { let x: Option<Int> = none; if(x)@@!some { return 0i; } return x; }", "0_i");
+    });
 });
