a66b245b1fafe383b22e97af911fa84a50bc8d4a
Mark Marron <marron@cs.uky.edu>
2025-07-05 20:25:23 -0400

    Fixes and tests (#257)
    Notable:
    1. Fix for invoke rcvr -- make sure when calling an method defined on GeneralType (e.g. concept) the rcvr argument is boxed.
    2. Fix on binder ifs with datatype -- in special case where there are 2 possibilities then make sure the else case refines down as well as the if case -- e.g. `datatype Foo of F1 {} | F2 {}; ... let x: Foo = ...; if(x)@<F1> then /*$x is F1*/ else /*$x is F2*/;`

--- test/smtoutput/cons_exps/cons_entity.test.js
+++ test/smtoutput/cons_exps/cons_entity.test.js
@@ -9,6 +9,17 @@ describe ("SMT evaluate -- Entity Constructor", () => {
         runishMainCodeUnsat('entity Foo { field f: Int; field g: Bool; } public function main(k: Int): Bool { return Foo{k, true}.g; }', "(assert (not (Main@main 3)))");
         runishMainCodeUnsat('entity Foo { field f: Int; field g: Bool; } public function main(k: Int): Int { return Foo{k, true}.f; }', "(assert (not (= (Main@main 3) 3)))");
     });
+
+    it("should smt exec nominal", function () {
+        runishMainCodeUnsat('entity Foo { field f: Int; } public function main(): Int { return Foo{f = 1i}.f; }', "(assert (not (= 1 Main@main)))");
+        runishMainCodeUnsat('entity Foo { field f: Int; field g: Bool; } public function main(): Bool { return Foo{1i, g = true}.g; }', "(assert (not Main@main))");
+        runishMainCodeUnsat('entity Foo { field f: Int; field g: Bool; } public function main(): Bool { return Foo{f=1i, g = true}.g; }', "(assert (not Main@main))");
+    });
+
+    it("should smt exec default", function () {
+        runishMainCodeUnsat('entity Foo { field f: Int = 0i; } public function main(): Int { return Foo{}.f; }', "(assert (not (= Main@main 0)))");
+        runishMainCodeUnsat('entity Foo { field f: Int = 0i; } public function main(): Int { return Foo{5i}.f; }', "(assert (not (= Main@main 5)))");
+    });
 });
 
 describe ("SMT evaluate -- Entity w/ Invariant Constructor", () => {
@@ -21,6 +32,13 @@ describe ("SMT evaluate -- Entity w/ Invariant Constructor", () => {
 
     it("should smt exec find error", function () {
         runishMainCodeSat('entity Foo { field x: Int; field y: Int; invariant $x >= $y; } public function main(k: Int): Int { let f = Foo{k, 3i}; return f.x + f.y; }', "(declare-const a Int) (assert (not (is-@Result-err (Main@main a))))");
+    });
+
+    it("should exec default", function () {
+        runishMainCodeUnsat('entity Foo { field f: Int = 0i; invariant $f != 3i; } public function main(): Int { return Foo{5i}.f; }', "(assert (not (= (@Result-ok 5) Main@main)))");
+        runishMainCodeUnsat('entity Foo { field f: Int = 0i; invariant $f != 3i; } public function main(): Int { return Foo{}.f; }', "(assert (not (= (@Result-ok 0) Main@main)))");
+
+        runishMainCodeUnsat('entity Foo { field f: Int = 0i; invariant $f != 3i; } public function main(): Int { return Foo{3i}.f; }', "(assert (not (is-@Result-err Main@main)))");
     });
 });
 
