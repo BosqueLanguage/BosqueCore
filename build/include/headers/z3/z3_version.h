a2000079295450fa6d93ed913765c520ba7ac7fb
Mark Marron <mark.marron@uky.edu>
2025-07-07 10:20:13 -0400

    take 1

--- build/include/headers/z3/z3_version.h
+++ build/include/headers/z3/z3_version.h
@@ -1,7 +1,7 @@
 // automatically generated file.
 #define Z3_MAJOR_VERSION   4
-#define Z3_MINOR_VERSION   13
-#define Z3_BUILD_NUMBER    4
+#define Z3_MINOR_VERSION   15
+#define Z3_BUILD_NUMBER    2
 #define Z3_REVISION_NUMBER 0
 
-#define Z3_FULL_VERSION    "Z3 4.13.4.0"
+#define Z3_FULL_VERSION    "Z3 4.15.2.0"
