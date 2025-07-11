7eae937db6e14b38282eb7f4f10793406638822e
1stub <stubbiegaming@gmail.com>
2025-07-11 12:34:12 -0400

    improve typefunc emission

--- src/frontend/bsqir_emit.ts
+++ src/frontend/bsqir_emit.ts
@@ -1827,11 +1827,21 @@ class BSQIREmitter {
             const ibase = this.emitExplicitInvokeDecl(fdecl, nskey, ikey, fmt);
             this.mapper = omap;
 
-            const typeUpdatedNsKey = `${nskey}::${ikey.split("::").slice(0,-1).join("::")}`;
-            const typeUpdatedIKey = `${nskey}::${ikey}`;
-            let cstrns = typeUpdatedNsKey.split('::').map(e => `'${e}'`);
+            const nsParts = nskey.split('::');
+            const iParts = ikey.split('::');
+            
+            let matchingParts = 0;
+            const minLength = Math.min(nsParts.length, iParts.length);
+            while (matchingParts < minLength && nsParts[matchingParts] === iParts[matchingParts]) {
+                matchingParts++;
+            } 
+            const combinedParts = [...nsParts, ...iParts.slice(matchingParts)];
 
-            this.typefuncs.push(`'${ikey}'<BSQAssembly::InvokeKey> => BSQAssembly::TypeFunctionDecl{ ${ibase}, completens=List<CString>{${cstrns}}, completeikey='${typeUpdatedIKey}'<BSQAssembly::InvokeKey>}`);
+            const updatedIKey = combinedParts.join("::");
+            const updatedNsKey = `${updatedIKey.split("::").slice(0,-2).join("::")}`; // Does not include type declared in or name
+            let cstrns = updatedNsKey.split('::').map(e => `'${e}'`);
+
+            this.typefuncs.push(`'${ikey}'<BSQAssembly::InvokeKey> => BSQAssembly::TypeFunctionDecl{ ${ibase}, completens=List<CString>{${cstrns}}, completeikey='${updatedIKey}'<BSQAssembly::InvokeKey>}`);
             this.allfuncs.push(`'${ikey}'<BSQAssembly::InvokeKey>`);        
         }
         else {
