96211fe03255f5ca62c4d78ca31ef2b2566c8045
Mark Marron <mark.marron@protonmail.com>
2025-07-12 22:36:28 -0400

    yep

--- test/smtoutput/readme/datatype.test.js
+++ test/smtoutput/readme/datatype.test.js
@@ -2,7 +2,7 @@
 
 import { runishMainCodeUnsat } from "../../../bin/test/smtoutput/smtemit_nf.js";
 import { describe, it } from "node:test";
-/*
+
 const dtype = 'datatype BoolOp of Const { val: Bool } | NotOp { arg: BoolOp } | AndOp { larg: BoolOp, rarg: BoolOp } | OrOp { larg: BoolOp, rarg: BoolOp } & { recursive method evaluate(): Bool { match(this)@ { Const  => { return $this.val; } | NotOp => { return !$this.arg.evaluate[recursive](); } | AndOp => { return $this.larg.evaluate[recursive]() && $this.rarg.evaluate[recursive](); } | OrOp  => { return $this.larg.evaluate[recursive]() || $this.rarg.evaluate[recursive](); } } } }';
 
 describe ("SMT datatype exec", () => {
@@ -11,5 +11,5 @@ describe ("SMT datatype exec", () => {
         runishMainCodeUnsat(`${dtype} public function main(): Bool { return AndOp{larg=Const{true}, rarg=Const{false}}.evaluate[recursive](); }`, "(assert (not (= (@Result-ok false) Main@main)))");
     });
 });
-*/
+
 
