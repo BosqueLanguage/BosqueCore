8c845eb7bb16cb8c565eaf024fc4df30fc2c85a5
Mark Marron <mark.marron@uky.edu>
2025-07-15 15:37:22 -0400

    yep

--- test/smtoutput/match_stmts/simple_match.test.js
+++ test/smtoutput/match_stmts/simple_match.test.js
@@ -5,10 +5,10 @@ import { describe, it } from "node:test";
 
 describe ("SMT -- match Statement", () => {
     it("should smt exec simple match", function () {
-        runishMainCodeUnsat("public function main(): Int { let x: Option<Int> = some(3i); match(x) { None => { return 0i; } | _ => { return 1i; } } }", "(assert (not (= (@Result-ok 1) Main@main)))");
-        runishMainCodeUnsat("public function main(): Int { let x: Option<Int> = none; match(x) { None => { return 0i; } | _ => { return 1i; } } }", "(assert (not (= (@Result-ok 0) Main@main)))");
+        runishMainCodeUnsat("public function main(): Int { let x: Option<Int> = some(3i); match(x) { None => { return 0i; } | _ => { return 1i; } } }", "(assert (not (= 1 Main@main)))");
+        runishMainCodeUnsat("public function main(): Int { let x: Option<Int> = none; match(x) { None => { return 0i; } | _ => { return 1i; } } }", "(assert (not (= 0 Main@main)))");
 
-        runishMainCodeUnsat("datatype Foo of F1 {} | F2 {}; public function main(): Int { let x: Foo = F1{}; match(x) { F1 => { return 0i; } | F2 => { return 1i; } } }", "(assert (not (= (@Result-ok 0) Main@main)))");
+        runishMainCodeUnsat("datatype Foo of F1 {} | F2 {}; public function main(): Int { let x: Foo = F1{}; match(x) { F1 => { return 0i; } | F2 => { return 1i; } } }", "(assert (not (= 0 Main@main)))");
     });
 
     it("should smt exec fail simple match", function () {
@@ -16,9 +16,9 @@ describe ("SMT -- match Statement", () => {
     });
 
     it("should smt exec binder match", function () {
-        runishMainCodeUnsat("public function main(): Int { let x: Option<Int> = some(3i); match(x)@ { None => { return 0i; } | _ => { return 1i; } } }", "(assert (not (= (@Result-ok 1) Main@main)))");
+        runishMainCodeUnsat("public function main(): Int { let x: Option<Int> = some(3i); match(x)@ { None => { return 0i; } | _ => { return 1i; } } }", "(assert (not (= 1 Main@main)))");
 
-        runishMainCodeUnsat("datatype Foo of F1 {} | F2 { g: Int }; public function main(): Int { let x: Foo = F1{}; match(x)@ { F1 => { return 0i; } | F2 => { return $x.g; } } }", "(assert (not (= (@Result-ok 0) Main@main)))");
-        runishMainCodeUnsat("datatype Foo of F1 {} | F2 { g: Int }; public function main(): Int { let x: Foo = F2{5i}; match(x)@ { F1 => { return 0i; } | F2 => { return $x.g; } } }", "(assert (not (= (@Result-ok 5) Main@main)))");
+        runishMainCodeUnsat("datatype Foo of F1 {} | F2 { g: Int }; public function main(): Int { let x: Foo = F1{}; match(x)@ { F1 => { return 0i; } | F2 => { return $x.g; } } }", "(assert (not (= 0 Main@main)))");
+        runishMainCodeUnsat("datatype Foo of F1 {} | F2 { g: Int }; public function main(): Int { let x: Foo = F2{5i}; match(x)@ { F1 => { return 0i; } | F2 => { return $x.g; } } }", "(assert (not (= 5 Main@main)))");
     });
 });
