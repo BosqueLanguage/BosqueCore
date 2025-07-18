9a934efbac1e96210da68c449d874cb60cbfa3d7
1stub <stubbiegaming@gmail.com>
2025-07-18 11:21:26 -0400

    add tests

--- test/cppoutput/list/list_map.test.js
+++ test/cppoutput/list/list_map.test.js
@@ -2,3 +2,43 @@
 
 import { runMainCode } from "../../../bin/test/cppoutput/cppemit_nf.js";
 import { describe, it } from "node:test";
+
+describe ("CPP Emit Evaluate List -- map basic", () => {
+    it("should do simple maps", function () {
+        runMainCode('public function main(): Nat { return List<Int>{}.map<Bool>(fn(x) => x >= 0i).size(); }', "0_n");
+
+        runMainCode('public function main(): Bool { return List<Int>{1i}.map<Bool>(fn(x) => x >= 0i).front(); }', "true");
+        runMainCode('public function main(): Bool { return List<Int>{1i, -1i}.map<Bool>(fn(x) => x >= 0i).back(); }', "false");
+    });
+
+/*
+    it("should do simple maps w/err", function () {
+        runMainCode('public function main(): Nat { return List<Int>{}.map<Bool>(fn(x) => { assert x != 0i; return x >= 0i; }).size(); }', "(assert (not (= (@Result-ok 0) Main@main)))");
+
+        runMainCode('public function main(): Bool { return List<Int>{1i}.map<Bool>(fn(x) => { assert x != 0i; return x >= 0i; }).front(); }', "(assert (not (= (@Result-ok true) Main@main)))");
+        runMainCode('public function main(): Bool { return List<Int>{1i, 0i, -1i}.map<Bool>(fn(x) => { assert x != 0i; return x >= 0i; }).back(); }', "(assert (not (is-@Result-err Main@main)))");
+    });
+*/
+});
+
+describe ("CPP Emit Evaluate List -- map index basic", () => {
+    it("should do simple maps index", function () {
+        runMainCode('public function main(): Nat { return List<Nat>{}.mapIdx<Bool>(fn(x, i) => x >= i).size(); }', "0_n");
+
+        runMainCode('public function main(): Bool { return List<Nat>{1n}.mapIdx<Bool>(fn(x, i) => x >= i).front(); }', "true");
+        runMainCode('public function main(): Bool { return List<Nat>{1n, 0n}.mapIdx<Bool>(fn(x, i) => x >= i).back(); }', "false");
+
+        runMainCode('public function main(): Nat { return List<Int>{1i, -1i, 3i}.mapIdx<Nat>(fn(x, i) => i).back(); }', "2_n");
+    });
+
+/*
+    it("should do simple maps index w/err", function () {
+        runMainCode('public function main(): Nat { return List<Nat>{}.mapIdx<Bool>(fn(x, i) => { assert x != 0n; return x >= i; }).size(); }', "(assert (not (= (@Result-ok 0) Main@main)))");
+
+        runMainCode('public function main(): Bool { return List<Nat>{1n}.mapIdx<Bool>(fn(x, i) => { assert x != 0n; return x >= i; }).front(); }', "(assert (not (= (@Result-ok true) Main@main)))");
+        runMainCode('public function main(): Bool { return List<Nat>{1n, 0n}.mapIdx<Bool>(fn(x, i) => { assert x != 0n; return x >= i; }).back(); }', "(assert (not (is-@Result-err Main@main)))");
+
+        runMainCode('public function main(): Nat { return List<Int>{1i, -1i, 3i}.mapIdx<Nat>(fn(x, i) => { assert x != 0i; return i; }).back(); }', "(assert (not (= (@Result-ok 2) Main@main)))");
+    });
+*/
+});
--- test/cppoutput/list/list_predof_ops.test.js
+++ test/cppoutput/list/list_predof_ops.test.js
@@ -2,3 +2,64 @@
 
 import { runMainCode } from "../../../bin/test/cppoutput/cppemit_nf.js";
 import { describe, it } from "node:test";
+
+
+describe ("CPP Emit Evaluate List -- allof basic", () => {
+    it("should do simple allof", function () {
+        runMainCode('public function main(): Bool { return List<Int>{}.allOf(pred(x) => x >= 0i); }', "true");
+        runMainCode('public function main(): Bool { return List<Int>{1i, 2i}.allOf(pred(x) => x >= 0i); }', "true");
+
+        runMainCode('public function main(): Bool { return List<Int>{-2i}.allOf(pred(x) => x >= 0i); }', "false");
+        runMainCode('public function main(): Bool { return List<Int>{1i, -1i}.allOf(pred(x) => x >= 0i); }', "false");
+    });
+
+/*
+    it("should smt do simple allof w/err", function () {
+        runMainCode('public function main(): Bool { return List<Int>{1i, 2i}.allOf(pred(x) => { assert x != 0i; return x >= 0i; }); }', "(assert (not (= (@Result-ok true) Main@main)))");
+        runMainCode('public function main(): Bool { return List<Int>{-2i, 1i}.allOf(pred(x) => { assert x != 0i; return x >= 0i; }); }', "(assert (not (= (@Result-ok false) Main@main)))");
+
+        runMainCode('public function main(): Bool { return List<Int>{1i, 0i, 3i}.allOf(pred(x) => { assert x != 0i; return x >= 0i; }); }', "(assert (not (is-@Result-err Main@main)))");
+        runMainCode('public function main(): Bool { return List<Int>{-1i, 0i, 3i}.allOf(pred(x) => { assert x != 0i; return x >= 0i; }); }', "(assert (not (= (@Result-ok false) Main@main)))");
+    });
+*/
+});
+
+describe ("CPP Emit Evaluate List -- noneof basic", () => {
+    it("should do simple noneof", function () {
+        runMainCode('public function main(): Bool { return List<Int>{}.noneOf(pred(x) => x >= 0i); }', "true");
+        runMainCode('public function main(): Bool { return List<Int>{-1i, -2i}.noneOf(pred(x) => x >= 0i); }', "true");
+
+        runMainCode('public function main(): Bool { return List<Int>{2i}.noneOf(pred(x) => x >= 0i); }', "false");
+        runMainCode('public function main(): Bool { return List<Int>{1i, -1i}.noneOf(pred(x) => x >= 0i); }', "false");
+    });
+
+/*
+    it("should smt do simple noneof w/err", function () {
+        runMainCode('public function main(): Bool { return List<Int>{-1i, -2i}.noneOf(pred(x) => { assert x != 0i; return x >= 0i; }); }', "(assert (not (= (@Result-ok true) Main@main)))");
+        runMainCode('public function main(): Bool { return List<Int>{1i, -1i}.noneOf(pred(x) => { assert x != 0i; return x >= 0i; }); }', "(assert (not (= (@Result-ok false) Main@main)))");
+
+        runMainCode('public function main(): Bool { return List<Int>{-1i, 0i, 3i}.noneOf(pred(x) => { assert x != 0i; return x >= 0i; }); }', "(assert (not (is-@Result-err Main@main)))");
+        runMainCode('public function main(): Bool { return List<Int>{1i, 0i, 3i}.noneOf(pred(x) => { assert x != 0i; return x >= 0i; }); }', "(assert (not (= (@Result-ok false) Main@main)))");
+    });
+*/
+});
+
+describe ("CPP Emit Evaluate List -- someof basic", () => {
+    it("should do simple someof", function () {
+        runMainCode('public function main(): Bool { return List<Int>{}.someOf(pred(x) => x >= 0i); }', "false");
+        runMainCode('public function main(): Bool { return List<Int>{-1i, 2i}.someOf(pred(x) => x >= 0i); }', "true");
+
+        runMainCode('public function main(): Bool { return List<Int>{-2i}.someOf(pred(x) => x >= 0i); }', "false");
+        runMainCode('public function main(): Bool { return List<Int>{-1i, -1i}.someOf(pred(x) => x >= 0i); }', "false");
+    });
+
+/*
+    it("should smt do simple someof w/err", function () {
+        runMainCode('public function main(): Bool { return List<Int>{-1i, 2i}.someOf(pred(x) => { assert x != 0i; return x >= 0i; }); }', "(assert (not (= (@Result-ok true) Main@main)))");
+        runMainCode('public function main(): Bool { return List<Int>{-1i, -1i}.someOf(pred(x) => { assert x != 0i; return x >= 0i; }); }', "(assert (not (= (@Result-ok false) Main@main)))");
+
+        runMainCode('public function main(): Bool { return List<Int>{-1i, 0i, 3i}.someOf(pred(x) => { assert x != 0i; return x >= 0i; }); }', "(assert (not (is-@Result-err Main@main)))");
+        runMainCode('public function main(): Bool { return List<Int>{1i, 0i, 3i}.someOf(pred(x) => { assert x != 0i; return x >= 0i; }); }', "(assert (not (= (@Result-ok true) Main@main)))");
+    });
+*/
+});
