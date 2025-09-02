e1caf03495a39e34f3f1cd0adf62911571233d31
Anthony Arnold <100536399+1stub@users.noreply.github.com>
2025-09-02 08:36:22 -0400

    Map Tests and Bug Fixes (#327)
    * add tests and bug fixes
    
    * test reformatting, still many broke :/
    
    * ok i think good
    
    * goodbye bandage

---
+++ test/cppoutput/map/map_map.test.js
@@ -0,0 +1,14 @@
+"use strict";
+
+import { runMainCode } from "../../../bin/test/cppoutput/cppemit_nf.js"
+import { describe, it } from "node:test";
+
+describe ("CPP Emit Evaluate -- Map map basic", () => {
+    it("should do simple maps", function () {
+        runMainCode('public function main(): Nat { return Map<Int, Bool>{}.map<Bool>(fn(k, v) => v).size(); }', "0_n");
+
+        runMainCode('public function main(): Bool { return Map<Int, Int>{1i => 1i}.map<Bool>(fn(k, v) => k == v).get(1i); }', "true");
+        runMainCode('public function main(): Bool { return Map<Int, Int>{1i => 1i, 2i => 1i}.map<Bool>(fn(k, v) => k == v).get(1i); }', "true");
+        runMainCode('public function main(): Bool { return Map<Int, Int>{1i => 1i, 2i => 1i}.map<Bool>(fn(k, v) => k == v).get(2i); }', "false");
+    });
+});
