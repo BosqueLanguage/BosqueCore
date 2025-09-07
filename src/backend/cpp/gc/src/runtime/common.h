430b56f92d301ed503ece8bd3cc43233ebd1c3f8
1stub <stubbiegaming@gmail.com>
2025-09-07 10:52:05 -0400

    promotion mstat

--- src/backend/cpp/gc/src/runtime/common.h
+++ src/backend/cpp/gc/src/runtime/common.h
@@ -266,6 +266,7 @@ static_assert(sizeof(MetaData) == 8, "MetaData size is not 8 bytes");
 #define TOTAL_ALLOC_MEMORY(E)     (E).mstats.total_alloc_memory
 #define TOTAL_LIVE_BYTES(E)       (E).mstats.total_live_bytes
 #define TOTAL_COLLECTIONS(E)      (E).mstats.total_collections
+#define TOTAL_PROMOTIONS(E)       (E).mstats.total_promotions
 #define MIN_COLLECTION_TIME(E)    (E).mstats.min_collection_time
 #define MAX_COLLECTION_TIME(E)    (E).mstats.max_collection_time
 #define MAX_LIVE_HEAP(E)          (E).mstats.max_live_heap
@@ -274,6 +275,7 @@ static_assert(sizeof(MetaData) == 8, "MetaData size is not 8 bytes");
 #define UPDATE_TOTAL_ALLOC_MEMORY(E, OP, ...)     TOTAL_ALLOC_MEMORY((E)) OP __VA_ARGS__
 #define UPDATE_TOTAL_LIVE_BYTES(E, OP, ...)       TOTAL_LIVE_BYTES((E)) OP __VA_ARGS__
 #define UPDATE_TOTAL_COLLECTIONS(E, OP, ...)      TOTAL_COLLECTIONS((E)) OP __VA_ARGS__
+#define UPDATE_TOTAL_PROMOTIONS(E, OP, ...)      TOTAL_PROMOTIONS((E)) OP __VA_ARGS__
 #define UPDATE_MIN_COLLECTION_TIME(E, OP, ...)    MIN_COLLECTION_TIME((E)) OP __VA_ARGS__
 #define UPDATE_MAX_COLLECTION_TIME(E, OP, ...)    MAX_COLLECTION_TIME((E)) OP __VA_ARGS__
 #define UPDATE_MAX_LIVE_HEAP(E, OP, ...)          MAX_LIVE_HEAP((E)) OP __VA_ARGS__
@@ -290,6 +292,7 @@ static_assert(sizeof(MetaData) == 8, "MetaData size is not 8 bytes");
 #define UPDATE_TOTAL_ALLOC_MEMORY(E, OP, ...)
 #define UPDATE_TOTAL_LIVE_BYTES(E, OP, ...)
 #define UPDATE_TOTAL_COLLECTIONS(E, OP, ...)
+#define UPDATE_TOTAL_PROMOTIONS(E, OP, ...)
 #define UPDATE_MIN_COLLECTION_TIME(E, OP, ...)
 #define UPDATE_MAX_COLLECTION_TIME(E, OP, ...)
 #define UPDATE_MAX_LIVE_HEAP(E, OP, ...)
