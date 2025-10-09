8a82adb646dc61c519e5c8995b5e6f2bd663c236
Mark Marron <mark.marron@uky.edu>
2025-10-09 13:52:20 -0400

    stdlib lambda position update

--- test/stdlib/list/list_reduce.test.js
+++ test/stdlib/list/list_reduce.test.js
@@ -5,47 +5,47 @@ import { describe, it } from "node:test";
 
 describe ("List -- reduce simple", () => {
     it("should do simple bool reduce", function () {
-        runMainCode('public function main(): Bool { return List<Int>{}.reduce<Bool>(true, fn(acc, x) => acc && x > 0i); }', "true");
-        runMainCode('public function main(): Bool { return List<Int>{}.reduce<Bool>(false, fn(acc, x) => acc && x > 0i); }', "false");
+        runMainCode('public function main(): Bool { return List<Int>{}.reduce<Bool>(fn(acc, x) => acc && x > 0i, true); }', "true");
+        runMainCode('public function main(): Bool { return List<Int>{}.reduce<Bool>(fn(acc, x) => acc && x > 0i, false); }', "false");
 
-        runMainCode('public function main(): Bool { return List<Int>{1i, 3i}.reduce<Bool>(true, fn(acc, x) => acc && x > 0i); }', "true");
-        runMainCode('public function main(): Bool { return List<Int>{2i, 0i, 3i}.reduce<Bool>(true, fn(acc, x) => acc && x > 0i); }', "false");
+        runMainCode('public function main(): Bool { return List<Int>{1i, 3i}.reduce<Bool>(fn(acc, x) => acc && x > 0i, true); }', "true");
+        runMainCode('public function main(): Bool { return List<Int>{2i, 0i, 3i}.reduce<Bool>(fn(acc, x) => acc && x > 0i, true); }', "false");
     });
 
     it("should do simple int reduce", function () {
-        runMainCode('public function main(): Int { return List<Int>{}.reduce<Int>(0i, fn(acc, x) => acc + x); }', "0i");
-        runMainCode('public function main(): Int { return List<Int>{}.reduce<Int>(5i, fn(acc, x) => acc + x); }', "5i");
+        runMainCode('public function main(): Int { return List<Int>{}.reduce<Int>(fn(acc, x) => acc + x, 0i); }', "0i");
+        runMainCode('public function main(): Int { return List<Int>{}.reduce<Int>(fn(acc, x) => acc + x, 5i); }', "5i");
 
-        runMainCode('public function main(): Int { return List<Int>{1i, 3i}.reduce<Int>(0i, fn(acc, x) => acc + x); }', "4i");
-        runMainCode('public function main(): Int { return List<Int>{-2i, 0i, 3i}.reduce<Int>(0i, fn(acc, x) => acc + x); }', "1i");
+        runMainCode('public function main(): Int { return List<Int>{1i, 3i}.reduce<Int>(fn(acc, x) => acc + x, 0i); }', "4i");
+        runMainCode('public function main(): Int { return List<Int>{-2i, 0i, 3i}.reduce<Int>(fn(acc, x) => acc + x, 0i); }', "1i");
     });
 
     it("should do simple int lreduce", function () {
-        runMainCode('public function main(): Int { return List<Int>{}.lreduce<Int>(0i, fn(acc, x) => acc + x); }', "0i");
-        runMainCode('public function main(): Int { return List<Int>{}.lreduce<Int>(5i, fn(acc, x) => acc + x); }', "5i");
+        runMainCode('public function main(): Int { return List<Int>{}.lreduce<Int>(fn(acc, x) => acc + x, 0i); }', "0i");
+        runMainCode('public function main(): Int { return List<Int>{}.lreduce<Int>(fn(acc, x) => acc + x, 5i); }', "5i");
 
-        runMainCode('public function main(): Int { return List<Int>{1i, 3i}.lreduce<Int>(0i, fn(acc, x) => acc + x); }', "4i");
-        runMainCode('public function main(): Int { return List<Int>{-2i, 0i, 3i}.lreduce<Int>(0i, fn(acc, x) => acc + x); }', "1i");
+        runMainCode('public function main(): Int { return List<Int>{1i, 3i}.lreduce<Int>(fn(acc, x) => acc + x, 0i); }', "4i");
+        runMainCode('public function main(): Int { return List<Int>{-2i, 0i, 3i}.lreduce<Int>(fn(acc, x) => acc + x, 0i); }', "1i");
     });
 });
 
 describe ("List -- transduce simple", () => {
     it("should do simple int transduce", function () {
-        runMainCode('public function main(): Int { return List<Int>{}.transduce<Int, Bool>(0i, fn(e, x) => (|e + 1i, x > 2i|)).0; }', "0i");
-        runMainCode('public function main(): Nat { return List<Int>{}.transduce<Int, Bool>(0i, fn(e, x) => (|e + 1i, x > 2i|)).1.size(); }', "0n");
+        runMainCode('public function main(): Int { return List<Int>{}.transduce<Int, Bool>(fn(e, x) => (|e + 1i, x > 2i|), 0i).0; }', "0i");
+        runMainCode('public function main(): Nat { return List<Int>{}.transduce<Int, Bool>(fn(e, x) => (|e + 1i, x > 2i|), 0i).1.size(); }', "0n");
 
-        runMainCode('public function main(): Int { return List<Int>{1i, 2i, 3i}.transduce<Int, Bool>(0i, fn(e, x) => (|e + 1i, x > 2i|)).0; }', "3i");
-        runMainCode('public function main(): Nat { return List<Int>{1i, 2i, 3i}.transduce<Int, Bool>(0i, fn(e, x) => (|e + 1i, x > 2i|)).1.size(); }', "3n");
-        runMainCode('public function main(): Bool { return List<Int>{1i, 2i, 3i}.transduce<Int, Bool>(0i, fn(e, x) => (|e + 1i, x > 2i|)).1.front(); }', "false");
-        runMainCode('public function main(): Bool { return List<Int>{1i, 2i, 3i}.transduce<Int, Bool>(0i, fn(e, x) => (|e + 1i, x > 2i|)).1.back(); }', "true");
+        runMainCode('public function main(): Int { return List<Int>{1i, 2i, 3i}.transduce<Int, Bool>(fn(e, x) => (|e + 1i, x > 2i|), 0i).0; }', "3i");
+        runMainCode('public function main(): Nat { return List<Int>{1i, 2i, 3i}.transduce<Int, Bool>(fn(e, x) => (|e + 1i, x > 2i|), 0i).1.size(); }', "3n");
+        runMainCode('public function main(): Bool { return List<Int>{1i, 2i, 3i}.transduce<Int, Bool>(fn(e, x) => (|e + 1i, x > 2i|), 0i).1.front(); }', "false");
+        runMainCode('public function main(): Bool { return List<Int>{1i, 2i, 3i}.transduce<Int, Bool>(fn(e, x) => (|e + 1i, x > 2i|), 0i).1.back(); }', "true");
     });
 
     it("should do simple int ltransduce", function () {
-        runMainCode('public function main(): Int { return List<Int>{}.ltransduce<Int, Bool>(0i, fn(e, x) => (|e + 1i, x > 2i|)).0; }', "0i");
-        runMainCode('public function main(): Nat { return List<Int>{}.ltransduce<Int, Bool>(0i, fn(e, x) => (|e + 1i, x > 2i|)).1.size(); }', "0n");
+        runMainCode('public function main(): Int { return List<Int>{}.ltransduce<Int, Bool>(fn(e, x) => (|e + 1i, x > 2i|), 0i).0; }', "0i");
+        runMainCode('public function main(): Nat { return List<Int>{}.ltransduce<Int, Bool>(fn(e, x) => (|e + 1i, x > 2i|), 0i).1.size(); }', "0n");
 
-        runMainCode('public function main(): Int { return List<Int>{1i, 2i, 3i}.ltransduce<Int, Bool>(0i, fn(e, x) => (|e + 1i, x > 2i|)).0; }', "3i");
-        runMainCode('public function main(): Nat { return List<Int>{1i, 2i, 3i}.ltransduce<Int, Bool>(0i, fn(e, x) => (|e + 1i, x > 2i|)).1.size(); }', "3n");
+        runMainCode('public function main(): Int { return List<Int>{1i, 2i, 3i}.ltransduce<Int, Bool>(fn(e, x) => (|e + 1i, x > 2i|), 0i).0; }', "3i");
+        runMainCode('public function main(): Nat { return List<Int>{1i, 2i, 3i}.ltransduce<Int, Bool>(fn(e, x) => (|e + 1i, x > 2i|), 0i).1.size(); }', "3n");
     });
 });
 
--- test/stdlib/map/map_reduce.test.js
+++ test/stdlib/map/map_reduce.test.js
@@ -5,19 +5,19 @@ import { describe, it } from "node:test";
 
 describe ("Map -- reduce simple", () => {
     it("should do simple bool reduce", function () {
-        runMainCode('public function main(): Bool { return Map<Int, Int>{}.reduce<Bool>(true, fn(acc, k, v) => acc && k > v); }', "true");
-        runMainCode('public function main(): Bool { return Map<Int, Int>{}.reduce<Bool>(false, fn(acc, k, v) => acc && v > 0i); }', "false");
+        runMainCode('public function main(): Bool { return Map<Int, Int>{}.reduce<Bool>(fn(acc, k, v) => acc && k > v, true); }', "true");
+        runMainCode('public function main(): Bool { return Map<Int, Int>{}.reduce<Bool>(fn(acc, k, v) => acc && v > 0i, false); }', "false");
 
-        runMainCode('public function main(): Bool { return Map<Int, Int>{1i => 2i, 3i => 4i}.reduce<Bool>(true, fn(acc, k, v) => acc && v > 0i); }', "true");
-        runMainCode('public function main(): Bool { return Map<Int, Int>{1i => 2i, 3i => 0i}.reduce<Bool>(true, fn(acc, k, v) => acc && v > 0i); }', "false");
-        runMainCode('public function main(): Bool { return Map<Int, Int>{1i => 2i, 3i => 0i}.reduce<Bool>(true, fn(acc, k, v) => acc && k > 0i); }', "true");
+        runMainCode('public function main(): Bool { return Map<Int, Int>{1i => 2i, 3i => 4i}.reduce<Bool>(fn(acc, k, v) => acc && v > 0i, true); }', "true");
+        runMainCode('public function main(): Bool { return Map<Int, Int>{1i => 2i, 3i => 0i}.reduce<Bool>(fn(acc, k, v) => acc && v > 0i, true); }', "false");
+        runMainCode('public function main(): Bool { return Map<Int, Int>{1i => 2i, 3i => 0i}.reduce<Bool>(fn(acc, k, v) => acc && k > 0i, true); }', "true");
     });
 
     it("should do simple int reduce", function () {
-        runMainCode('public function main(): Int { return Map<Int, Int>{}.reduce<Int>(0i, fn(acc, k, v) => acc + v); }', "0i");
-        runMainCode('public function main(): Int { return Map<Int, Int>{}.reduce<Int>(5i, fn(acc, k, v) => acc + v); }', "5i");
+        runMainCode('public function main(): Int { return Map<Int, Int>{}.reduce<Int>(fn(acc, k, v) => acc + v, 0i); }', "0i");
+        runMainCode('public function main(): Int { return Map<Int, Int>{}.reduce<Int>(fn(acc, k, v) => acc + v, 5i); }', "5i");
 
-        runMainCode('public function main(): Int { return Map<Int, Int>{1i => 2i, 3i => 0i}.reduce<Int>(0i, fn(acc, k, v) => acc + k); }', "4i");
-        runMainCode('public function main(): Int { return Map<Int, Int>{1i => 2i, 3i => 0i}.reduce<Int>(0i, fn(acc, k, v) => acc + v); }', "2i");
+        runMainCode('public function main(): Int { return Map<Int, Int>{1i => 2i, 3i => 0i}.reduce<Int>(fn(acc, k, v) => acc + k, 0i); }', "4i");
+        runMainCode('public function main(): Int { return Map<Int, Int>{1i => 2i, 3i => 0i}.reduce<Int>(fn(acc, k, v) => acc + v, 0i); }', "2i");
     });
 });
