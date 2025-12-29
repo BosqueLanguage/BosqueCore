dbdd67bf3a4275c4c4be110c4457a6c4895964fe
Mark Marron <mark.marron@protonmail.com>
2025-12-29 10:40:48 -0500

    bin exps with type alias parse

--- test/parser/bin_exps/addition.test.js
+++ test/parser/bin_exps/addition.test.js
@@ -1,10 +1,10 @@
 "use strict";
 
-import { parseTestExp, parseTestExpError } from "../../../bin/test/parser/parse_nf.js";
+import { parseTestExp, parseTestExpError, parseTestFunctionInFile } from "../../../bin/test/parser/parse_nf.js";
 import { describe, it } from "node:test";
 
 describe ("Parser -- Nat addition", () => {
-    it("should parse simple nats", function () {
+    it("should parse simple ops", function () {
         parseTestExp("0n + 1n", undefined, "Nat");
         parseTestExp("+2n + 2n", undefined, "Nat");
         parseTestExp("1n + +3n", undefined, "Nat");
@@ -13,10 +13,14 @@ describe ("Parser -- Nat addition", () => {
     it("should fail stuck signs", function () {
         parseTestExpError("2n+3n", 'Expected ";" but got "+3n" when parsing "line statement"', "Nat");
     });
+
+    it("should parse type alias ops", function () {
+        parseTestFunctionInFile("type Foo = Int; [FUNC]", "function main(): Foo { return 1i<Foo> + 2i<Foo>; }");
+    });
 });
 
 describe ("Parser -- ChkInt addition", () => {
-    it("should parse simple nats", function () {
+    it("should parse simple ops", function () {
         parseTestExp("0I + 1I", undefined, "ChkInt");
         parseTestExp("+2I + -2I", undefined, "ChkInt");
         parseTestExp("1I + +3I", undefined, "ChkInt");
