97790417af199c1be035ae43bb195503ec08a405
Mark Marron <mark.marron@protonmail.com>
2025-12-28 23:16:31 -0500

    composite arith test

---
+++ test/parser/bin_exps/composite.test.js
@@ -0,0 +1,20 @@
+"use strict";
+
+import { parseTestExp, parseTestExpError } from "../../../bin/test/parser/parse_nf.js";
+import { describe, it } from "node:test";
+
+describe ("Parser -- Int operation trees", () => {
+    it("should parse simple pm", function () {
+        parseTestExp("(0i + 1i) - 4i", undefined, "Int");
+        parseTestExp("(+2n + 2n) - 2n", undefined, "Nat");
+    });
+
+    it("should parse precedence pm", function () {
+        parseTestExp("0i + 1i * -4i", "0i + (1i * -4i)", "Int");
+        parseTestExp("(0i + 1i) * -4i", undefined, "Int");
+        parseTestExp("0i + (1i * -4i)", undefined, "Int");
+        parseTestExp("+2n // 2n - 2n", "(+2n // 2n) - 2n", "Nat");
+        parseTestExp("(+2n // 2n) - 2n", undefined, "Nat");
+        parseTestExp("+2n // (2n - 2n)", undefined, "Nat");
+    });
+});
