da4feef439906b6cf9cf440632fa4b014e00d0c6
Mark Marron <mark.marron@uky.edu>
2026-01-29 18:59:25 -0500

    parser overload test

--- test/parser/fcall_exps/ns_fcall_ps_exps.test.js
+++ test/parser/fcall_exps/ns_fcall_ps_exps.test.js
@@ -18,3 +18,14 @@ describe ("Parser -- NamespaceFunction Ref Params", () => {
         parseTestFunctionInFileError('entity Foo{ } function foo(ref y: Foo): Int { return 1i; } function main(): Int { return foo(ref Foo{}); }', "Expected variable as target in special passing argument");
     });
 });
+
+describe ("Parser -- NamespaceFunction Resolve Overload", () => {
+    it("should parse overloads and call", function () {
+        parseTestFunctionInFile('function foo(y: Int): Int { return y; } function foo(inout y: Int): Int { return y; } [FUNC]', 'function main(): Int { var i = 0i; return foo(i); }');
+        parseTestFunctionInFile('function foo(y: Int): Int { return y; } function foo<T>(y: Int): Int { return y; } [FUNC]', 'function main(): Int { var i = 0i; return foo<Int>(i); }');
+    });
+
+    it("should fail overloads and call", function () {
+        parseTestFunctionInFileError('function foo(y: Int): Int { return y; } function foo(y: Int, x: Int): Int { return y; } function main(): Int { var i = 0i; return foo(i); }', 'Collision between function and other names -- foo');
+    });
+});
