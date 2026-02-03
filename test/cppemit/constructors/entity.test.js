0cd239223b75a29276027d3eeff448d45c0eed71
Mark Marron <mark.marron@protonmail.com>
2026-02-02 19:57:25 -0500

    more tests

---
+++ test/cppemit/constructors/entity.test.js
@@ -0,0 +1,37 @@
+"use strict";
+
+import { checkTestEmitMainFunction } from "../../../bin/test/cppemit/cppemit_nf.js";
+import { describe, it } from "node:test";
+
+describe ("CPPEmit -- Entity Constructor", () => {
+    it("should emit positional", function () {
+        checkTestEmitMainFunction('entity Foo { field f: Int; } public function main(): Foo { return Foo{1i}; }', "MainᕒFoo Mainᕒmain() { return MainᕒFoo{1_i}; }");
+        checkTestEmitMainFunction('entity Foo { field f: Int; field g: Bool; } public function main(): Foo { return Foo{1i, false}; }', "MainᕒFoo Mainᕒmain() { return MainᕒFoo{1_i, FALSE}; }");
+    });
+
+    it("should emit nominal", function () {
+        checkTestEmitMainFunction('entity Foo { field f: Int; } public function main(): Foo { return Foo{f = 1i}; }', "MainᕒFoo Mainᕒmain() { return MainᕒFoo{1_i}; }");
+        checkTestEmitMainFunction('entity Foo { field f: Int; field g: Bool; } public function main(): Foo { return Foo{1i, g = true}; }', "MainᕒFoo Mainᕒmain() { return MainᕒFoo{1_i, TRUE}; }");
+        checkTestEmitMainFunction('entity Foo { field f: Int; field g: Bool; } public function main(): Foo { return Foo{f=1i, g = true}; }', "MainᕒFoo Mainᕒmain() { return MainᕒFoo{1_i, TRUE}; }");
+        checkTestEmitMainFunction('entity Foo { field f: Int; field g: Bool; } public function main(): Foo { return Foo{ g = true, f=1i}; }', "MainᕒFoo Mainᕒmain() { return MainᕒFoo{1_i, TRUE}; }");
+    });
+
+    it("should emit default", function () {
+        checkTestEmitMainFunction('entity Foo { field f: Int = 0i; } public function main(): Foo { return Foo{}; }', "MainᕒFoo Mainᕒmain() { return MainᕒFoo{0_i}; }");
+        checkTestEmitMainFunction('entity Foo { field f: Int = 0i; } public function main(): Foo { return Foo{5i}; }', "MainᕒFoo Mainᕒmain() { return MainᕒFoo{5_i}; }");
+    });
+});
+
+describe ("CPPEmit -- Entity w/ Invariant Constructor", () => {
+    it("should emit positional", function () {
+        checkTestEmitMainFunction('entity Foo { field f: Int; invariant $f > 3i; } public function main(): Foo { return Foo{4i}; }', 'MainᕒFoo Mainᕒmain() { ᐸRuntimeᐳ::bsq_invariant((bool)(MainᕒFooᐤinvariant_0(4_i)), "test.bsq", 2, nullptr, "Failed Invariant"); return MainᕒFoo{4_i}; }');
+        checkTestEmitMainFunction('entity Foo { field f: Int; field g: Bool; invariant $g ==> $f > 3i; } public function main(): Foo { return Foo{1i, false}; }', 'MainᕒFoo Mainᕒmain() { ᐸRuntimeᐳ::bsq_invariant((bool)(MainᕒFooᐤinvariant_0(1_i, FALSE)), "test.bsq", 2, nullptr, "Failed Invariant"); return MainᕒFoo{1_i, FALSE}; }');
+
+        checkTestEmitMainFunction('entity Foo { field f: Int; field g: Bool; invariant !$g; invariant $f != 0i; } public function main(): Foo { return Foo{1i, false}; }', 'MainᕒFoo Mainᕒmain() { ᐸRuntimeᐳ::bsq_invariant((bool)(MainᕒFooᐤinvariant_0(1_i, FALSE)), "test.bsq", 2, nullptr, "Failed Invariant"); ᐸRuntimeᐳ::bsq_invariant((bool)(MainᕒFooᐤinvariant_1(1_i, FALSE)), "test.bsq", 2, nullptr, "Failed Invariant"); return MainᕒFoo{1_i, FALSE}; }');
+    });
+
+    it("should emit default", function () {
+        checkTestEmitMainFunction('entity Foo { field f: Int = 0i; invariant $f != 3i; } public function main(): Foo { return Foo{5i}; }', 'MainᕒFoo Mainᕒmain() { ᐸRuntimeᐳ::bsq_invariant((bool)(MainᕒFooᐤinvariant_0(5_i)), "test.bsq", 2, nullptr, "Failed Invariant"); return MainᕒFoo{5_i}; }');
+        checkTestEmitMainFunction('entity Foo { field f: Int = 0i; invariant $f != 3i; } public function main(): Foo { return Foo{}; }', 'MainᕒFoo Mainᕒmain() { ᐸRuntimeᐳ::bsq_invariant((bool)(MainᕒFooᐤinvariant_0(0_i)), "test.bsq", 2, nullptr, "Failed Invariant"); return MainᕒFoo{0_i}; }');
+    });
+});
---
+++ test/cppemit/return_stmts/single_return.test.js
@@ -0,0 +1,35 @@
+"use strict";
+
+import { checkTestEmitMainFunction } from "../../../bin/test/cppemit/cppemit_nf.js";
+import { describe, it } from "node:test";
+
+
+describe ("CPPEmit -- simple return", () => {
+    it("should emit simple returns", function () {
+        checkTestEmitMainFunction('public function main(x: Int): Int { return x; }', 'Int Mainᕒmain(Int x) { return x; }');
+        checkTestEmitMainFunction('entity Foo { field f: Int; } public function main(x: Foo): Foo { return x; }', 'MainᕒFoo Mainᕒmain(MainᕒFoo x) { return x; }');
+
+        checkTestEmitMainFunction('public function main(x: Int): Bool { return x == 0i; }', 'Bool Mainᕒmain(Int x) { return x == 0_i; }');
+    });
+
+    it("should emit constructor returns", function () {
+        checkTestEmitMainFunction('entity Foo { field f: Int; } public function main(): Foo { return Foo{3i}; }', 'MainᕒFoo Mainᕒmain() { return MainᕒFoo{3_i}; }');
+        checkTestEmitMainFunction('entity Foo { field f: Int; invariant $f !== 0i; } public function main(): Foo { return Foo{3i}; }', 'MainᕒFoo Mainᕒmain() { ᐸRuntimeᐳ::bsq_invariant((bool)(MainᕒFooᐤinvariant_0(3_i)), "test.bsq", 2, nullptr, "Failed Invariant"); return MainᕒFoo{3_i}; }');
+        
+        //TODO -- later add a ref version
+    });
+
+    it.skip("should emit direct returns", function () {
+        checkTestEmitMainFunction('public function foo(x: Int): Int { return x + 1i; } public function main(): Int { return foo(3i); }', 'iii');
+        checkTestEmitMainFunction('concept Baz {} entity Foo provides Baz { field f: Int; } public function foo(x: Int): Foo { return Foo{x + 1i}; } public function main(): Baz { return foo(3i); }', 'jjj');
+    });
+
+    it("should emit returns with special coerce", function () {
+        checkTestEmitMainFunction('public function main(): Option<Int> { return none; }', 'OptionᐸIntᐳ Mainᕒmain() { return OptionᐸIntᐳ::optnone; }');
+        checkTestEmitMainFunction('public function main(): Option<Int> { return some(3i); }', 'OptionᐸIntᐳ Mainᕒmain() { return OptionᐸIntᐳ::fromSome(&ᐸRuntimeᐳ::g_typeinfo_SomeᐸIntᐳ, SomeᐸIntᐳ{3_i}); }');
+        checkTestEmitMainFunction('public function main(): Option<Int> { let x: Option<Int> = some(3i); return x; }', 'OptionᐸIntᐳ Mainᕒmain() { OptionᐸIntᐳ x = OptionᐸIntᐳ::fromSome(&ᐸRuntimeᐳ::g_typeinfo_SomeᐸIntᐳ, SomeᐸIntᐳ{3_i}); return x; }');
+
+        checkTestEmitMainFunction('concept Baz {} entity Foo provides Baz {} public function main(): Baz { return Foo{}; }', 'ppp');
+        checkTestEmitMainFunction('concept Baz {} entity Foo provides Baz {} public function main(): Baz { let x: Foo = Foo{}; return x; }', 'qqq');
+    });
+});
