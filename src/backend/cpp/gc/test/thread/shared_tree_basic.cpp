2ef8042805cca099511a6770a51bca3878cc13ef
Anthony Arnold <stubbiegaming@gmail.com>
2026-02-03 18:15:14 -0500

    more

--- src/backend/cpp/gc/test/thread/shared_tree_basic.cpp
+++ src/backend/cpp/gc/test/thread/shared_tree_basic.cpp
@@ -7,33 +7,61 @@ do { \
 	𝐚𝐬𝐬𝐞𝐫𝐭(g_memstats.total_live_bytes == 0); \
 }while(0)
 
-__CoreCpp::Int sharedBasicTreeTest_1()
+// this assumes we are only testing with two live threads, may be interesting
+// to try spawning some more and see if things still work
+template<size_t N>
+static void threadTest(void* roots[N], size_t nthds) noexcept
 {
-	void* root0 = Main::accessNode(Main::makeTree(1_n, 0_n));
-	void* root1 = Main::accessNode(Main::makeTree(1_n, 0_n));
-	gtl_info.thd_testing_data[0] = root0;
-	gtl_info.thd_testing_data[1] = root1;
+	for(size_t i = 0; i < N; i++) {
+		gtl_info.thd_testing_data[i] = roots[i];
+	}
+    	
+	initializeGC<sizeof(allocs) / sizeof(allocs[0])>(allocs, gtl_info);
 	collect();
 
+	for(size_t i = 0; i < N; i++) {
+		MetaData* m = GC_GET_META_DATA_ADDR(roots[i]);
+		𝐚𝐬𝐬𝐞𝐫𝐭(static_cast<size_t>(GC_THREAD_COUNT(m)) == nthds);
+	}
+}
+
+// would be interesting to add these as methods to a new type we use for 
+// the thread data stuff
+template <size_t N>
+static void insert(void* roots[N]) noexcept
+{
+	static_assert(N < static_cast<size_t>(NUM_THREAD_TESTING_ROOTS));	
+	for(size_t i = 0; i < N; i++) {
+		gtl_info.thd_testing_data[i] = roots[i];
+	}
+}
+
+static constexpr void clear() noexcept
+{
+	for(unsigned i = 0; i < NUM_THREAD_TESTING_ROOTS; i++) {
+		gtl_info.thd_testing_data[i] = nullptr;	
+	}
+}
+
+// roots dropped during BSQMemoryTheadLocalInfo destruction
+
+__CoreCpp::Int sharedBasicTreeTest_1()
+{	
+	const size_t nroots = 2, nthds = 2;
+	void* roots[nroots] = { 
+		Main::accessNode(Main::makeTree(1_n, 0_n)),
+		Main::accessNode(Main::makeTree(1_n, 0_n))
+	};
+	insert<nroots>(roots);
+
+	collect();
 	𝐚𝐬𝐬𝐞𝐫𝐭(g_memstats.total_live_bytes > 0);
 
-	std::thread thd = std::thread([root0, root1]() {
-		gtl_info.thd_testing_data[0] = root0;
-		gtl_info.thd_testing_data[1] = root1;
-    	initializeGC<sizeof(allocs) / sizeof(allocs[0])>(allocs, gtl_info);
-		
-		collect();
-
-		MetaData* m0 = GC_GET_META_DATA_ADDR(root0);
-		MetaData* m1 = GC_GET_META_DATA_ADDR(root1);
-		𝐚𝐬𝐬𝐞𝐫𝐭(GC_THREAD_COUNT(m0) == 2 && GC_THREAD_COUNT(m1) == 2);
-
-		// roots dropped during BSQMemoryTheadLocalInfo destruction
+	std::thread thd = std::thread([troots = roots]() {
+		threadTest<nroots>(troots, nthds);	
 	});
 
-	gtl_info.thd_testing_data[0] = nullptr;
-	gtl_info.thd_testing_data[1] = nullptr;
-
+	clear();
 	verifyTest(thd);
 
 	return 1_i;
