d59ebf58bf6f031e016c8dc99fccb7f4f051915e
Anthony Arnold <stubbiegaming@gmail.com>
2026-02-05 19:00:56 -0500

    wide tree

---
+++ src/backend/cpp/gc/test/thread/shared_tree_wide.cpp
@@ -0,0 +1,119 @@
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+//
+// TODO need to figure out how i dont have to copy all of this every test...
+// maybe we just put everything in one file? for all tests?
+//
+
+std::mutex g_mtx;
+std::condition_variable g_cv;
+bool g_pause = true;	
+
+// roots dropped during BSQMemoryTheadLocalInfo destruction
+template <size_t N>
+static void threadTest(void* testroots[N], size_t nthds) noexcept
+{
+	for(size_t i = 0; i < N; i++) {
+		MetaData* m = GC_GET_META_DATA_ADDR(testroots[i]);
+		𝐚𝐬𝐬𝐞𝐫𝐭(static_cast<size_t>(GC_THREAD_COUNT(m)) == nthds && GC_IS_ALLOCATED(m));
+	}
+}
+
+template <size_t N>
+static void runThreadTest(size_t nthds, void* roots[N])
+{	
+	gtl_info.insertThreadTestData<N>(roots);
+	gtl_info.collectfp();
+	𝐚𝐬𝐬𝐞𝐫𝐭(g_memstats.total_live_bytes > 0);
+
+	std::thread thd = std::thread([troots = roots, nthds]() {
+		for(size_t i = 0; i < N; i++) {
+			gtl_info.thd_testing_data[i] = troots[i];
+		} 
+		initializeGC<sizeof(allocs) / sizeof(allocs[0])>(allocs, gtl_info, collect);
+
+		threadTest<N>(troots, nthds);
+	
+		std::unique_lock lk(g_mtx);
+		g_pause = false;
+		lk.unlock();
+		g_cv.notify_one();
+
+		lk.lock();
+		g_cv.wait(lk, []{ return g_pause; });
+
+		// main thread dropped the roots, make sure no one died
+		threadTest<N>(troots, nthds - 1);
+	});
+
+	std::unique_lock lk(g_mtx);
+	g_cv.wait(lk, []{ return !g_pause; });
+
+	gtl_info.clearThreadTestData();
+	gtl_info.collectfp();
+	g_pause = true;
+	lk.unlock();
+	g_cv.notify_one();
+
+	thd.join();
+	gtl_info.collectfp();
+	𝐚𝐬𝐬𝐞𝐫𝐭(g_memstats.total_live_bytes == 0);
+}
+
+__CoreCpp::Int sharedWideTreeTest_1()
+{	
+	const size_t nroots = 2, nthds = 2;
+	void* roots[nroots] = { 
+		Main::accessNode(Main::makeTree(1_n, 0_n)),
+		Main::accessNode(Main::makeTree(1_n, 0_n))
+	};
+	runThreadTest<nroots>(nthds, roots);
+
+	return 1_i;
+}
+
+__CoreCpp::Int sharedWideTreeTest_2()
+{	
+	const size_t nroots = 2, nthds = 2;
+	void* roots[nroots] = { 
+		Main::accessNode(Main::makeTree(2_n, 0_n)),
+		Main::accessNode(Main::makeTree(2_n, 0_n))
+	};
+	runThreadTest<nroots>(nthds, roots);
+
+	return 1_i;
+}
+
+__CoreCpp::Int sharedWideTreeTestMulti_1()
+{	
+	const size_t nroots = 6, nthds = 2;
+	void* roots[nroots] = { 
+		Main::accessNode(Main::makeTree(1_n, 0_n)),
+		Main::accessNode(Main::makeTree(1_n, 0_n)),
+		Main::accessNode(Main::makeTree(1_n, 0_n)),
+		Main::accessNode(Main::makeTree(1_n, 0_n)),
+		Main::accessNode(Main::makeTree(1_n, 0_n)),
+		Main::accessNode(Main::makeTree(1_n, 0_n))
+	};
+	runThreadTest<nroots>(nthds, roots);
+
+	return 1_i;
+}
+
+__CoreCpp::Int sharedWideTreeTestMulti_2()
+{	
+	const size_t nroots = 6, nthds = 2;
+	void* roots[nroots] = { 
+		Main::accessNode(Main::makeTree(2_n, 0_n)),
+		Main::accessNode(Main::makeTree(2_n, 0_n)),
+		Main::accessNode(Main::makeTree(2_n, 0_n)),
+		Main::accessNode(Main::makeTree(2_n, 0_n)),
+		Main::accessNode(Main::makeTree(2_n, 0_n)),
+		Main::accessNode(Main::makeTree(2_n, 0_n))
+	};
+	runThreadTest<nroots>(nthds, roots);
+
+	return 1_i;
+}
