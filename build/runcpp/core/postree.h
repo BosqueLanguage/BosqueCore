d1edfa9cba80c0e9ca3a8240087a70364799b035
Mark Marron <mark.marron@protonmail.com>
2026-02-21 23:13:41 -0500

    yes

--- build/runcpp/core/postree.h
+++ build/runcpp/core/postree.h
@@ -21,7 +21,11 @@ namespace ᐸRuntimeᐳ
         Node
     };
 
-    template<typename T, size_t K> class PosRBTree;
+    template<typename T, size_t K> class PosRBTreeNode;
+
+    class PosRBTreeLeafEmpty 
+    {
+    };
 
     template<typename T, size_t K> 
     class PosRBTreeLeaf
@@ -31,49 +35,57 @@ namespace ᐸRuntimeᐳ
         T data[K];
     };
 
+    template<typename T, size_t K>
+    union PosRBTreeUnion
+    {
+        PosRBTreeLeafEmpty bbleaf;
+        PosRBTreeLeaf<T, K>* leaf;
+        PosRBTreeNode<T, K>* node;
+
+        constexpr PosRBTreeUnion() : leaf() {}
+        constexpr PosRBTreeUnion(const PosRBTreeUnion& other) = default;
+        constexpr PosRBTreeUnion(PosRBTreeLeafEmpty l) : bbleaf(l) {}
+        constexpr PosRBTreeUnion(PosRBTreeLeaf<T, K>* l) : leaf(l) {}
+        constexpr PosRBTreeUnion(PosRBTreeNode<T, K>* n) : node(n) {}
+    };
+    template<typename T, size_t K>
+    using PosRBTreeRepr = BoxedUnion<PosRBTreeUnion<T, K>>;
+
     template<typename T, size_t K> 
     class PosRBTreeNode
     {
     public:
         size_t count;
         RColor color;
-        PosRBTree<T, K>* left;
-        PosRBTree<T, K>* right;
+        PosRBTreeRepr<T, K> left;
+        PosRBTreeRepr<T, K> right;
     };
-
-    template<typename T, size_t K>
-    union PosRBTreeUnion
-    {
-        PosRBTreeLeaf<T, K> leaf;
-        PosRBTreeNode<T, K> node;
-
-        constexpr PosRBTreeUnion() : leaf() {}
-        constexpr PosRBTreeUnion(const PosRBTreeUnion& other) = default;
-        constexpr PosRBTreeUnion(PosRBTreeLeaf<T, K>* l) : leaf(l) {}
-        constexpr PosRBTreeUnion(PosRBTreeNode<T, K>* n) : node(n) {}
-    };    
 
     template<typename T, size_t K>
     class PosRBTree
     {
     public:
-        PosRBTreeTag tag;
-        PosRBTreeUnion<T, K> cstrtree;
-
+        static TypeInfo* bbleaftype;
+        static TypeInfo* leaftype;
+        static TypeInfo* nodetype;
+        
+        PosRBTreeRepr<T, K> repr;
+        
         constexpr size_t count() const
         {
-            if(this->tag == PosRBTreeTag::Leaf) {
-                return this->cstrtree.leaf.count;
+            if(tree->repr.xxxx == PosRBTreeTag::Leaf) {
+                return tree->repr.cstrtree.leaf.count;
             }
             else {
-                return this->cstrtree.node.count;
+                return tree->cstrtree.node.count;
             }
         }
 
-        PosRBTreeLeaf<T, K>* getLeaf(size_t index) const
+        template<typename T, size_t K>
+        PosRBTreeLeaf<T, K>* getLeaf(const PosRBTree<T, K>* tree, size_t index) const
         {
-            if(this->tag == PosRBTreeTag::Leaf) {
-                return &this->cstrtree.leaf;
+            if(tree->tag == PosRBTreeTag::Leaf) {
+                return &tree->cstrtree.leaf;
             }
             else {
                 assert(false); // Not Implemented: full getLeaf for PosRBTree
@@ -81,10 +93,11 @@ namespace ᐸRuntimeᐳ
             }
         }
 
-        T& get(size_t index)
+        template<typename T, size_t K>
+        T& get(const PosRBTree<T, K>* tree, size_t index)
         {
-            if(this->tag == PosRBTreeTag::Leaf) {
-                return this->cstrtree.leaf.data[index];
+            if(tree->tag == PosRBTreeTag::Leaf) {
+                return tree->cstrtree.leaf.data[index];
             }
             else {
                 assert(false); // Not Implemented: full get for PosRBTree
