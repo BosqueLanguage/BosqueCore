e81f378c863b40f55a1e14c166f977b18341de76
Mark Marron <mark.marron@uky.edu>
2026-03-20 14:13:16 -0400

    field access type check

--- src/frontend/checker.ts
+++ src/frontend/checker.ts
@@ -2546,7 +2546,6 @@ class TypeChecker {
     ////////
     // Postfix Expressions
     private checkPostfixAccessFromName(env: TypeEnvironment, exp: PostfixAccessFromName, rcvrtype: TypeSignature): TypeSignature {
-        /*
         const finfo = this.relations.resolveTypeField(rcvrtype, exp.name, this.constraints);
         if(finfo === undefined) {
             this.reportError(exp.sinfo, `Could not find field ${exp.name} in type ${rcvrtype.emit()}`);
@@ -2558,8 +2557,6 @@ class TypeChecker {
         }
 
         return exp.setType(finfo.member.declaredType.remapTemplateBindings(finfo.typeinfo.mapping));
-        */
-        assert(false, "Not Implemented -- checkPostfixAccessFromName");
     }
 
     private checkPostfixProjectFromNames(env: TypeEnvironment, exp: PostfixProjectFromNames, rcvrtype: TypeSignature, infertype: TypeInferContext | undefined): TypeSignature {
