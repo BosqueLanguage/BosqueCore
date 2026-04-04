bbb5de14e968895377f451378d5ea7e5b25b3a93
Mark Marron <mark.marron@uky.edu>
2026-04-04 13:46:19 -0400

    lambda

--- test/cppemit/lambda/lambda_basic.test.js
+++ test/cppemit/lambda/lambda_basic.test.js
@@ -11,3 +11,11 @@ describe ("CPPEmit -- Lambda calls (no template, no capture)", () => {
         checkTestEmitMainFunction("function foo(f: fn(Int) -> Int, z: Int): Int { return f(z); } public function main(y: Int): Int { return foo(fn(x) => x + 5i, y) + foo(fn(x) => x * 2i, y); }", 'Int Mainᕒmain(Int y) { Int tmp_0 = Mainᕒfooᑅfn_0ᑀ(fn_0_ldata_{}, y); Int tmp_1 = Mainᕒfooᑅfn_1ᑀ(fn_1_ldata_{}, y); ᐸRuntimeᐳ::XInt::checkOverflowAddition(tmp_0, tmp_1, "test.bsq", 2); return tmp_0 + tmp_1; }');
     });
 });
+
+describe ("CPPEmit -- Lambda calls (with template)", () => {
+    it("should emit simple lambda template", function () {
+        checkTestEmitMainFunction("function foo<T>(x: T, f: fn(T) -> T): T { return f(x); } public function main(): Int { return foo<Int>(3i, fn(x) => x); }", 'ccc');
+        checkTestEmitMainFunction("function foo<T>(x: T, f: fn(T) -> T): T { return f(x); } public function main(): Nat { if(foo<Int>(3i, fn(x) => x) > 0i) { return foo<Nat>(3n, fn(x) => x); } return 0n; }", 'ccc');
+    });
+});
+
--- test/cppemit/lambda/lambda_capture.test.js
+++ test/cppemit/lambda/lambda_capture.test.js
@@ -6,12 +6,10 @@ import { describe, it } from "node:test";
 describe ("CPPEmit -- Lambda calls (no template)", () => {
     it("should emit lambda capture", function () {
         checkTestEmitMainFunction("function foo(f: fn(Int) -> Int): Int { return f(1i); } public function main(): Int { let y = 1i; return foo(fn(x) => { return x + y; }); }", 'Int Mainᕒmain() { Int y = 1_i; return Mainᕒfooᑅfn_0ᑀ(fn_0_ldata_{y}); }');
+        checkTestEmitMainFunction("function foo(f: fn(Int) -> Int): Int { return f(1i); } public function main(z: Int): Int { let y = 1i; return foo(fn(x) => { return x + y + z; }); }", 'Int Mainᕒmain(Int z) { Int y = 1_i; return Mainᕒfooᑅfn_0ᑀ(fn_0_ldata_{y, z}); }');
+    });
 
-        /*
-        checkTestFunctionInFile('function foo(f: fn(Int) -> Int): Int { return f(1i); } function bar(g: fn(Int) -> Int, k: Int): Int { return foo(fn(y) => g(y) + k); } public function main(): Int { let y = 1i; return bar(fn(x) => { return x + y; }, 2i); }');
-
-        checkTestFunctionInFile('function foo(f: fn(Int) -> Int): Int { return f(1i); } public function main(g: fn(Int) -> Int): Int { let y = 1i; return foo(g); }');
-        checkTestFunctionInFile('function foo(f: fn(Int) -> Int): Int { return f(1i); } public function main(g: fn(Int) -> Int): Int { let y = 1i; return foo(fn(x) => { return g(x) + y; }); }');
-        */
+    it("should emit nested lambda capture", function () {
+        checkTestEmitMainFunction('function foo(f: fn(Int) -> Int): Int { return f(1i); } function bar(g: fn(Int) -> Int, k: Int): Int { return foo(fn(y) => g(y) + k); } public function main(): Int { let y = 1i; return bar(fn(x) => { return x + y; }, 2i); }', 'zzzz');
     });
 });
