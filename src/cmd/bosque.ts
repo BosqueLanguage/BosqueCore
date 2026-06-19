c6556837e0463254360d94abc368ef9ca114e45b
Mark Marron <mark.marron@protonmail.com>
2026-06-18 22:44:49 -0400

    stdlib

--- src/cmd/bosque.ts
+++ src/cmd/bosque.ts
@@ -70,6 +70,7 @@ function emitCommandLineMakefile(optlevel: "testing" | "release"): string {
         `BUILD := ${optlevel}\n\n` + 
         'CPP_STDFLAGS=-Wall -Wextra -Wno-unused-parameter -Wno-unused-variable -Wno-unused-but-set-variable -Wuninitialized -Werror -std=gnu++23 -fno-omit-frame-pointer -fno-exceptions -fno-rtti -fno-strict-aliasing -fno-stack-protector -fPIC\n' + 
         'CPPFLAGS_OPT.testing=-O0 -g -ggdb -fsanitize=address --param asan-stack=0\n' +
+        'CPPFLAGS_OPT.testingopt=-O1 -g -ggdb -fsanitize=address --param asan-stack=0\n' +
         'CPPFLAGS_OPT.release=-O1 -g -ggdb -march=x86-64-v3 -Wno-array-bounds -Wno-stringop-overflow\n' +
         'CPPFLAGS=${CPPFLAGS_OPT.${BUILD}} ${CPP_STDFLAGS}\n\n' +
         'HEADERS=$(wildcard $(SRC_DIR)*.h) $(wildcard $(CORE_SRC_DIR)*.h) $(wildcard $(RUNTIME_SRC_DIR)*.h) $(wildcard $(ALLOC_SRC_DIR)*.h) $(wildcard $(BSQIR_SRC_DIR)*.h)\n' +
