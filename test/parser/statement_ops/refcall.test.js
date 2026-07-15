912ddb3ebab9be6bf0de1ff43a54ed37004850ed
Mark Marron <mark.marron@protonmail.com>
2026-07-14 22:54:23 -0400

    ref parser tests

--- test/parser/statement_ops/refcall.test.js
+++ test/parser/statement_ops/refcall.test.js
@@ -4,4 +4,8 @@ import { parseTestFunctionInFile, parseTestFunctionInFileError } from "../../../
 import { describe, it } from "node:test";
 
 describe ("Parser -- ref call statements", () => {
+    it("should parse ref call statements", function () {
+        parseTestFunctionInFile('entity Foo { field x: Int; ref method foo() { return; } } [FUNC]', 'function main(ref z: Foo): Int { ref z[x = 5i]; return this.x; }');
+        parseTestFunctionInFile('entity Foo { field x: Int; ref method foo() { ref this[x = $x + 1i]; return; } } [FUNC]', 'function main(): Int { ref z = Foo{3i}; ref z.foo(); return z.x; }');
+    });
 });
--- test/parser/statement_ops/refupdate.test.js
+++ test/parser/statement_ops/refupdate.test.js
@@ -1,14 +1,19 @@
 "use strict";
 
-import { parseTestExp, parseTestFunction, parseTestFunctionInFileError } from "../../../bin/test/parser/parse_nf.js";
+import { parseTestFunctionInFile, parseTestFunctionInFileError } from "../../../bin/test/parser/parse_nf.js";
 import { describe, it } from "node:test";
 
 describe ("Parser -- ref updates", () => {
     it("should parse ref updates", function () {
-        parseTestFunction('function foo(ref x: Int): Int { return x; }', undefined);
+        parseTestFunctionInFile('entity Foo { field x: Int; } [FUNC]', 'function main(ref z: Foo): Int { ref z[x = 5i]; return this.x; }');
+        parseTestFunctionInFile('entity Foo { field x: Int; } [FUNC]', 'function main(ref z: Foo): Int { ref z[x = $x + 1i]; return this.x; }');
     });
 
     it("should fail ref updates", function () {
-        parseTestFunctionInFileError('function foo(ref x: Int): Int { return x; } function main(): Int { var y: Int = 0i; return foo(ref x = 1i); }', "Could not resolve 'x' in this context");
+        parseTestFunctionInFileError('entity Foo { field x: Int; } function foo(ref z: Foo): Int { z[x = 5i]; return this.x; }', 'Expected a call expression');
+        parseTestFunctionInFileError('entity Foo { field x: Int; } function ref foo(): Int { ref this[x: 5i]; return this.x; }', 'Expected "[IDENTIFIER]" but got "ref" when parsing "function name"');
+
+        parseTestFunctionInFileError('entity Foo { field x: Int; } function foo(ref z: Foo): Int { ref z[x = 5i; return this.x; }', 'Expected "]" but got "[RECOVER]" when parsing "variable update list"');
+        parseTestFunctionInFileError('entity Foo { field x: Int; } function foo(ref z: Foo): Int { ref z.[x = 5i]; return this.x; }', 'Expected "[IDENTIFIER]" but got "[" when parsing "ref invoke"');
     });
 });
