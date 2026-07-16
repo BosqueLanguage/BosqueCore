d9bd1eb22629445f70ef75f42c7ee0d84c6a9621
Mark Marron <mark.marron@uky.edu>
2026-07-16 11:27:54 -0400

    more of that

--- test/cppemit/statement_ops/refupdate.test.js
+++ test/cppemit/statement_ops/refupdate.test.js
@@ -14,6 +14,19 @@ describe ("CPPEmit -- ref updates", () => {
         checkTestEmitMainFunction('entity Foo { field x: Int; field y: Bool; } public function main(x: Int): Int { ref z = Foo{x, false}; ref z[y = true, x = 9i]; return z.x; }', 'eee');
     });
 
+    it("should emit ref updates direct with inherits/template", function () {
+        checkTestEmitMainFunction('concept Bar { field g: Bool; } entity Foo provides Bar { field x: Int; } public function main(x: Int): Int { var z = Foo{true, x}; ref z[x = 5i]; return z.x; }', 'lll');
+        checkTestEmitMainFunction('concept Bar { field g: Bool; } entity Foo provides Bar { field x: Int; } public function main(x: Int): Bool { var z = Foo{true, x}; ref z[g = false]; return z.g; }', 'lllw');
+
+        checkTestEmitMainFunction('entity Foo<T> { field x: T; } public function main(x: Int): Int { var z = Foo<Int>{x}; ref z[x = 5i]; return z.x; }', 'mmm');
+
+        checkTestEmitMainFunction('concept Bar<T> { field g: T; } entity Foo { field x: Int; } public function main(x: Int): Int { var z = Foo<Bool>{true, x}; ref z[x = 5i]; return z.x; }', 'nnn');
+        checkTestEmitMainFunction('concept Bar<T> { field g: T; } entity Foo<T> provides Bar<T> { field x: Int; } public function main(x: Int): Bool { var z = Foo<Bool>{true, x}; ref z[g = z.x == 0i]; return z.g; }', 'nnnw');
+    });
+
+    it("should emit ref updates direct with invariants", function () {
+    });
+
     it("should emit ref updates as params", function () {
         checkTestEmitMainFunction('entity Foo { field x: Int; } public function main(ref z: Foo): Int { ref z[x = 5i]; return z.x; }', 'fff');
         checkTestEmitMainFunction('entity Foo { field x: Int; ref method f() { ref this[x = 0i]; return; } } public function main(): Int { ref z = Foo{3i}; ref z.f(); return z.x; }', 'ggg');
