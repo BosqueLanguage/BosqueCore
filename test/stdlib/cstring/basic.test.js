1cc772832b9ed1298cbd5da36d49bfd250b712e1
Mark Marron <mark.marron@protonmail.com>
2026-07-31 21:40:17 -0400

    big strings

--- test/stdlib/cstring/basic.test.js
+++ test/stdlib/cstring/basic.test.js
@@ -14,3 +14,9 @@ describe ("CString -- immediate and size", () => {
         runTestSet('public function main(z: CString): Nat { return z.size(); }', [['""', '0n'], ['"a"', '1n'], ['"abc"', '3n']], []);
     });
 });
+
+describe ("CString -- big parse", () => {
+    it("should test big parsing", function () {
+        runTestSet('public function main(z: CString): CString { return z; }', [['"abcdefghijklmonpqrstuvwxyz!!!1234567890bigbrowndog"', "'abcdefghijklmonpqrstuvwxyz!!!1234567890bigbrowndog'"]], []);
+    });
+});
