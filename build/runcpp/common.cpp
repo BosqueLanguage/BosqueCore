178156e43ef4b70988beb454f55c92b5cc01399f
Mark Marron <mark.marron@uky.edu>
2026-08-27 13:04:49 -0400

    more

--- build/runcpp/common.cpp
+++ build/runcpp/common.cpp
@@ -50,29 +50,62 @@ namespace ᐸRuntimeᐳ
     {
         assert(bytecount != 1);
 
-        if(bytecount == 2) {
-            if((inbuff[0] & 0xC0) != 0xC0 || (inbuff[1] & 0xC0) != 0x80) {
+        //TODO: we need to review this invalid encoding setup 
+
+        // 2-byte (0xC2 - 0xDF) -- 0xC0 and 0xC1 are strictly overlong
+        if(inbuff[0] >= 0xC2 && inbuff[0] <= 0xDF) {
+            if ((inbuff[1] & 0xC0) != 0x80) {
                 return std::numeric_limits<char32_t>::max();
             }
-            else {
-                return (char32_t)((inbuff[0] & 0x1F) << 6 | (inbuff[1] & 0x3F));
+        }
+
+        // 3-byte sequence (0xE0 - 0xEF)
+        if (inbuff[0] >= 0xE0 && inbuff[0] <= 0xEF) {
+            if((inbuff[1] & 0xC0) != 0x80 || (inbuff[2] & 0xC0) != 0x80) {
+                return std::numeric_limits<char32_t>::max();
             }
+
+            // Overlong check: If b1 == 0xE0, b2 must be >= 0xA0
+            if(inbuff[0] == 0xE0 && inbuff[1] < 0xA0) {
+                return std::numeric_limits<char32_t>::max();
+            }
+            
+            // Surrogate pair rejection (0xED 0xA0 0x80 to 0xED 0xBF 0xBF)
+            if (inbuff[0] == 0xED && inbuff[1] >= 0xA0) {
+                return std::numeric_limits<char32_t>::max();
+            }
+        }
+
+        // 4-byte sequence (0xF0 - 0xF4)
+        if(inbuff[0] >= 0xF0 && inbuff[0] <= 0xF4) {
+            uint8_t b2 = static_cast<uint8_t>(inbuff[1]);
+            uint8_t b3 = static_cast<uint8_t>(inbuff[2]);
+            uint8_t b4 = static_cast<uint8_t>(inbuff[3]);
+
+            if ((inbuff[1] & 0xC0) != 0x80 || (inbuff[2] & 0xC0) != 0x80 || (inbuff[3] & 0xC0) != 0x80) {
+                return std::numeric_limits<char32_t>::max();
+            }
+
+            // Overlong check: If inbuff[0] == 0xF0, b2 must be >= 0x90
+            if (inbuff[0] == 0xF0 && inbuff[1] < 0x90) {
+                return std::numeric_limits<char32_t>::max();
+            }
+            
+            // Code point limit check: UTF-8 bounds end at 0xF4 0x8F 0xBF 0xBF (U+10FFFF)
+            if (inbuff[0] == 0xF4 && inbuff[1] > 0x8F) {
+                return std::numeric_limits<char32_t>::max();
+            }
+
+        }
+
+        if(bytecount == 2) {
+            return (char32_t)((inbuff[0] & 0x1F) << 6 | (inbuff[1] & 0x3F));
         }
         else if(bytecount == 3) {
-            if((inbuff[0] & 0xE0) != 0xE0 || (inbuff[1] & 0xC0) != 0x80 || (inbuff[2] & 0xC0) != 0x80) {
-                return std::numeric_limits<char32_t>::max();
-            }
-            else {
-                return (char32_t)((inbuff[0] & 0x0F) << 12 | ((inbuff[1] & 0x3F) << 6) | (inbuff[2] & 0x3F));
-            }
+            return (char32_t)((inbuff[0] & 0x0F) << 12 | ((inbuff[1] & 0x3F) << 6) | (inbuff[2] & 0x3F));
         }
         else {
-            if((inbuff[0] & 0xF0) != 0xF0 || (inbuff[1] & 0xC0) != 0x80 || (inbuff[2] & 0xC0) != 0x80 || (inbuff[3] & 0xC0) != 0x80) {
-                return std::numeric_limits<char32_t>::max();
-            }
-            else {
-                return (char32_t)((inbuff[0] & 0x07) << 18 | ((inbuff[1] & 0x3F) << 12) | ((inbuff[2] & 0x3F) << 6) | (inbuff[3] & 0x3F));
-            }
+            return (char32_t)((inbuff[0] & 0x07) << 18 | ((inbuff[1] & 0x3F) << 12) | ((inbuff[2] & 0x3F) << 6) | (inbuff[3] & 0x3F));
         }
     }
 }
