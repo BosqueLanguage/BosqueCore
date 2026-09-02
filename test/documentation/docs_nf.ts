f96252b8a3bccd49dfc9d2d1427fd316f41b0f9d
Mark Marron <mark.marron@protonmail.com>
2026-09-01 21:02:48 -0400

    working on build

--- test/documentation/docs_nf.ts
+++ test/documentation/docs_nf.ts
@@ -62,6 +62,8 @@ function emitCommandLineMakefile(): string {
         'ALLOC_SRC_DIR=$(RUNTIME_SRC_DIR)allocator/\n' +
         'BSQIR_SRC_DIR=$(RUNTIME_SRC_DIR)bsqir/\n' +
         '\n' +
+        'JSON_INCLUDES=-I $(MAKE_PATH)/json/\n' +
+        '\n' +
         'CPPFLAGS=-O0 -g -ggdb -fsanitize=address --param asan-stack=0 -Wall -Wextra -Wno-unused-parameter -Wno-unused-variable -Wno-unused-but-set-variable -Wuninitialized -Werror -std=gnu++23 -fno-omit-frame-pointer -fno-exceptions -fno-rtti -fno-strict-aliasing -fno-stack-protector\n' + 
         'LINKAGE=-lboost_regex -licuuc -licui18n -licudata\n' +
         'HEADERS=$(wildcard $(SRC_DIR)*.h) $(wildcard $(CORE_SRC_DIR)*.h) $(wildcard $(RUNTIME_SRC_DIR)*.h) $(wildcard $(ALLOC_SRC_DIR)*.h) $(wildcard $(BSQIR_SRC_DIR)*.h)\n' +
@@ -69,7 +71,7 @@ function emitCommandLineMakefile(): string {
         '\n' +
         'all: $(MAKE_PATH)/app\n\n' +
         '$(MAKE_PATH)/app: $(HEADERS) $(CPP) $(MAKE_PATH)/app.h $(MAKE_PATH)/app.cpp\n' +
-        '\tg++ $(CPPFLAGS) -o $(MAKE_PATH)/app $(CPP) $(MAKE_PATH)/app.cpp $(LINKAGE)\n'
+        '\tg++ $(CPPFLAGS) $(JSON_INCLUDES) -o $(MAKE_PATH)/app $(CPP) $(MAKE_PATH)/app.cpp $(LINKAGE)\n'
         ;
 }
 
